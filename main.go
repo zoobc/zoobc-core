6bc249b70de1189916b34365b707dde78dc85529
sukrawidhyawan <g.sukrawidhyawan02@gmail.com>
2019-07-10 16:13:07 +0800

    add func UpdateBlacklistedStatus

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func p2pService() {
 
 	// p2p Thread
 	go p2p.NewHostService(new(chaintype.MainChain)).ResolvePeersThread()
+	go p2p.NewHostService(new(chaintype.MainChain)).UpdateBlacklistedStatus()
 }
 
 func startServices(queryExecutor *query.Executor) {
