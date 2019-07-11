9de34585e23ef6e6f91a89baba73ec959ff4c81e
astaphobia <ariasta.ketut@icloud.com>
2019-07-11 19:18:18 +0800

    this commit to validate spendablebalance with amount

--- common/transaction/sendMoney.go
+++ common/transaction/sendMoney.go
@@ -1,16 +1,21 @@
 package transaction
 
 import (
+	"database/sql"
 	"errors"
+
+	"github.com/zoobc/zoobc-core/common/query"
 
 	"github.com/zoobc/zoobc-core/common/model"
 )
 
 type SendMoney struct {
-	Body               *model.SendMoneyTransactionBody
-	SenderAccountID    []byte
-	RecipientAccountID []byte
-	Heigh              uint32
+	Body                *model.SendMoneyTransactionBody
+	SenderAccountID     []byte
+	RecipientAccountID  []byte
+	Heigh               uint32
+	AccountBalanceQuery query.AccountBalanceQuery
+	QueryExecutor       query.ExecutorInterface
 }
 
 func (tx *SendMoney) Apply() error {
@@ -21,6 +26,11 @@ func (tx *SendMoney) Unconfirmed() error {
 	return nil
 }
 func (tx *SendMoney) Validate() error {
+	var (
+		rows           *sql.Rows
+		err            error
+		accountBalance *model.AccountBalance
+	)
 	if tx.Body.GetAmount() <= 0 {
 		return errors.New("transaction must have an amount more than 0")
 	}
@@ -31,6 +41,26 @@ func (tx *SendMoney) Validate() error {
 		if tx.SenderAccountID == nil {
 			return errors.New("transaction must hav a valid sender account id")
 		}
+
+		rows, err = tx.QueryExecutor.ExecuteSelect(tx.AccountBalanceQuery.GetAccountBalanceByAccountID())
+		if err != nil {
+			return err
+		}
+		err = rows.Scan(
+			&accountBalance.ID,
+			&accountBalance.BlockHeight,
+			&accountBalance.SpendableBalance,
+			&accountBalance.Balance,
+			&accountBalance.PopRevenue,
+		)
+		if err != nil {
+			return err
+		}
+
+		if accountBalance.SpendableBalance < tx.Body.GetAmount() {
+			return errors.New("transaction amount not enough")
+		}
+
 	}
 
 	return nil
