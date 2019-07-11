d8416ca0996eb57234185161f0832d847d93cda6
Andy Shi <shi88.andy@gmail.com>
2019-07-11 19:14:39 +0800

    45 generate account (#49)
    * #46 update migration
    
    * #45  generate account id method
    
    * #45 move node secret phrase to config
    
    * #45 move rpc and http endpoint port to config.toml
    
    * #45 update accounBalance ID to account ID
    
    * #45 update schema commit

--- common/database/migration.go
+++ common/database/migration.go
@@ -65,7 +65,7 @@ func (m *Migration) Init(qe *query.Executor) error {
 			);`,
 			`
 			CREATE TABLE IF NOT EXISTS "account" (
-				"id"	BLOB,
+				"account_id"	BLOB,
 				"account_type"	INTEGER,
 				"address"	TEXT,
 				PRIMARY KEY("id")
@@ -100,36 +100,6 @@ func (m *Migration) Init(qe *query.Executor) error {
 				"payload_hash" BLOB,
 				PRIMARY KEY("id")
 			);`,
-			`
-			CREATE TABLE IF NOT EXISTS "main_block" (
-				"id" INTEGER,
-				"previous_block_hash" BLOB,
-				"height" INTEGER,
-				"timestamp" INTEGER,
-				"block_seed" BLOB,
-				"block_signature" BLOB,
-				"cumulative_difficulty" VARCHAR,
-				"smith_scale" INTEGER,
-				"payload_length" INTEGER,
-				"payload_hash" BLOB,
-				"blocksmith_id" BLOB,
-				"total_amount" INTEGER,
-				"total_fee" INTEGER,
-				"total_coinbase" INTEGER,
-				"version" INTEGER,
-				PRIMARY KEY("id")
-			)
-			`,
-			`
-			CREATE TABLE IF NOT EXISTS "account_balance" (
-				"account_id"	BLOB,
-				"balance"	INTEGER,
-				"spendable_balance"	INTEGER,
-				"pop_revenue"	INTEGER,
-				"block_height"	INTEGER,
-				"latest"	INTEGER
-			);
-			`,
 		}
 		return nil
 	}
---
+++ common/util/account.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"encoding/base64"
+	"encoding/binary"
+
+	"golang.org/x/crypto/sha3"
+)
+
+// GetAccountIDByPublicKey return the account ID byte which is the hash of
+// account type (int32) and the account address (default: base64(public key))
+// for type 0
+func GetAccountIDByPublicKey(accountType int32, publicKey []byte) []byte {
+	accountTypeByte := make([]byte, 4)
+	binary.LittleEndian.PutUint32(accountTypeByte, uint32(accountType))
+	var address string
+	if accountType == 0 { // default account type: zoobc
+		rawAddress := make([]byte, 33)
+		copy(rawAddress, publicKey)
+
+		// Add Checksum Byte to end
+		rawAddress[32] = GetChecksumByte(publicKey)
+		address = base64.URLEncoding.EncodeToString(publicKey)
+	}
+	digest := sha3.New256()
+	_, _ = digest.Write(accountTypeByte)
+	_, _ = digest.Write([]byte(address))
+	accountID := digest.Sum([]byte{})
+	return accountID
+}
+
+// GetChecksumByte Calculate a checksum byte from a collection of bytes
+// checksum 255 = 255, 256 = 0, 257 = 1 and so on.
+func GetChecksumByte(bytes []byte) byte {
+	n := len(bytes)
+	var a byte
+	for i := 0; i < n; i++ {
+		a += bytes[i]
+	}
+	return a
+}
