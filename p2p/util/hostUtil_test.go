c8980c8e186056c81869293823bd235b7e20841d
Hardi Suputra <jiksdi12@gmail.com>
2019-07-12 15:02:13 +0800

    Complete p2p HostUtil Test

--- p2p/util/hostUtil_test.go
+++ p2p/util/hostUtil_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 
@@ -21,7 +22,7 @@ func TestNewHost(t *testing.T) {
 	}{
 		// TODO: Add test cases.
 		{
-			name: "NewHostTest:one",
+			name: "NewHostTest:NewHost",
 			args: args{
 				address: "127.0.0.1",
 				port:    3000,
@@ -87,6 +88,49 @@ func TestNewHost(t *testing.T) {
 			got := NewHost(tt.args.address, tt.args.port, tt.args.knownPeers)
 			if strings.Compare(got.String(), tt.want.String()) != 0 {
 				t.Errorf("\n%v \n%v", got.String(), tt.want.String())
+			}
+		})
+	}
+}
+
+func TestGetAnyPeer(t *testing.T) {
+	type args struct {
+		hs *model.Host
+	}
+	tests := []struct {
+		name string
+		args args
+		want *model.Peer
+	}{
+		// TODO: Add test cases.
+		{
+			name: "GetANyPeersTest:GetMorePeer",
+			args: args{
+				hs: &model.Host{
+					Peers: map[string]*model.Peer{
+						"127.0.0.1:3000": &model.Peer{
+							Info: &model.Node{
+								SharedAddress: "127.0.0.1",
+								Address:       "127.0.0.1",
+								Port:          3000,
+							},
+						},
+					},
+				},
+			},
+			want: &model.Peer{
+				Info: &model.Node{
+					SharedAddress: "127.0.0.1",
+					Address:       "127.0.0.1",
+					Port:          3000,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetAnyPeer(tt.args.hs); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetAnyPeer() = %v, want %v", got, tt.want)
 			}
 		})
 	}
