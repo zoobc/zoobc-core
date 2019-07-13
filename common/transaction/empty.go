42b2cbeb258db4427dd371878959fafc1a0a2bbb
astaphobia <ariasta.ketut@icloud.com>
2019-07-13 17:19:13 +0800

    this commit to add empty transaction

--- common/transaction/empty.go
+++ common/transaction/empty.go
@@ -1,7 +1,6 @@
 package transaction
 
 import (
-	"github.com/zoobc/zoobc-core/common/contract"
 	"github.com/zoobc/zoobc-core/common/model"
 )
 
@@ -9,12 +8,12 @@ type TXEmpty struct {
 	Body *model.EmptyTransactionBody
 }
 
-func (tx *TXEmpty) Apply(chainType contract.ChainType) error {
+func (tx *TXEmpty) Apply() error {
 	return nil
 }
-func (tx *TXEmpty) Unconfirmed(chainType contract.ChainType) error {
+func (tx *TXEmpty) Unconfirmed() error {
 	return nil
 }
-func (tx *TXEmpty) Validate(chainType contract.ChainType) error {
+func (tx *TXEmpty) Validate() error {
 	return nil
 }
