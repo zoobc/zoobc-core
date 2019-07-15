221a67cc2968ba5308433eeafd6afa59cecb2741
capt4ce <ahmad.ali.abdilah@gmail.com>
2019-07-15 11:56:43 +0800

    fixing the test for TransactionApiService

--- api/service/transactionApiService_test.go
+++ api/service/transactionApiService_test.go
@@ -49,7 +49,7 @@ func Test_TransactionService_GetTransactions(t *testing.T) {
 		Offset: 0,
 	}
 
-	mockData := struct {
+	expectedData := struct {
 		Total        uint64
 		Count        uint32
 		Transactions []*model.Transaction
@@ -107,9 +107,9 @@ func Test_TransactionService_GetTransactions(t *testing.T) {
 		{
 			name: "GetTransactions:success",
 			want: &model.GetTransactionsResponse{
-				Transactions: mockData.Transactions,
-				Total:        mockData.Total,
-				Count:        mockData.Count,
+				Transactions: expectedData.Transactions,
+				Total:        expectedData.Total,
+				Count:        expectedData.Count,
 			},
 			wantErr: false,
 		},
@@ -118,7 +118,6 @@ func Test_TransactionService_GetTransactions(t *testing.T) {
 	chainType := chaintype.GetChainType(0)
 	transactionQuery := query.NewTransactionQuery(chainType)
 	queryStr := transactionQuery.GetTransactions(params.Limit, params.Offset)
-	totalRecordQuery := query.GetTotalRecordOfSelect(queryStr)
 
 	mock.ExpectQuery(queryStr).WillReturnRows(sqlmock.NewRows([]string{
 		"id",
@@ -137,7 +136,8 @@ func Test_TransactionService_GetTransactions(t *testing.T) {
 		"signature",
 	}).AddRow(1, 1, 1, 0, "abc", 0, "abc", 1, 1, 11000, []byte{}, 1, []byte{}, []byte{}).AddRow(2, 2, 2, 1, "bcd", 1, "bcd", 2, 2, 21000, []byte{}, 2, []byte{}, []byte{}))
 
-	mock.ExpectQuery(totalRecordQuery).WillReturnRows(sqlmock.NewRows([]string{"total_record"}).AddRow(2))
+	// mocking record count query
+	mock.ExpectQuery("SELECT count").WillReturnRows(sqlmock.NewRows([]string{"total_record"}).AddRow(2))
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -148,6 +148,85 @@ func Test_TransactionService_GetTransactions(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("TransactionService.GetTransactions() = %v, want %v", got, tt.want)
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
+
+func Test_TransactionService_GetTransaction(t *testing.T) {
+	params := &model.GetTransactionRequest{
+		ID: 1,
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("error while opening database connection")
+	}
+	defer db.Close()
+	instance := NewTransactionService(query.NewQueryExecutor(db))
+	defer ResetTransactionService()
+
+	tests := []struct {
+		name    string
+		want    *model.Transaction
+		wantErr bool
+	}{
+		{
+			name: "GetTransaction:success",
+			want: &model.Transaction{
+				ID:                      1,
+				BlockID:                 1,
+				Height:                  1,
+				SenderAccountType:       0,
+				SenderAccountAddress:    "abc",
+				RecipientAccountType:    0,
+				RecipientAccountAddress: "abc",
+				TransactionType:         1,
+				Fee:                     1,
+				Timestamp:               11000,
+				TransactionHash:         []byte{},
+				TransactionBodyLength:   1,
+				TransactionBodyBytes:    []byte{},
+				Signature:               []byte{},
+			},
+			wantErr: false,
+		},
+	}
+
+	chainType := chaintype.GetChainType(0)
+	transactionQuery := query.NewTransactionQuery(chainType)
+	queryStr := transactionQuery.GetTransaction(params.ID)
+
+	mock.ExpectQuery(queryStr).WillReturnRows(sqlmock.NewRows([]string{
+		"id",
+		"block_id",
+		"block_height",
+		"sender_account_type",
+		"sender_account_address",
+		"recipient_account_type",
+		"recipient_account_address",
+		"transaction_type",
+		"fee",
+		"timestamp",
+		"transaction_hash",
+		"transaction_body_length",
+		"transaction_body_bytes",
+		"signature",
+	}).AddRow(1, 1, 1, 0, "abc", 0, "abc", 1, 1, 11000, []byte{}, 1, []byte{}, []byte{}))
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := instance.GetTransaction(chainType, params)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TransactionService.GetTransaction() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TransactionService.GetTransaction() = %v, want %v", got, tt.want)
 			}
 
 			if err := mock.ExpectationsWereMet(); err != nil {
