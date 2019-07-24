2521aee52662c69da669bf3b731b3bdaa0035f54
komodo2662 <thomas.prabowo@blockchainzoo.com>
2019-07-24 17:10:05 +0800

    improve generate proof of ownership function

--- core/service/nodeAdminCoreService.go
+++ core/service/nodeAdminCoreService.go
@@ -1,35 +1,56 @@
 package service
 
 import (
+	"bytes"
 	"errors"
 
+	"github.com/spf13/viper"
+	"github.com/zoobc/zoobc-core/common/crypto"
 	"github.com/zoobc/zoobc-core/common/model"
 	"github.com/zoobc/zoobc-core/common/query"
+	commonUtil "github.com/zoobc/zoobc-core/common/util"
+	"github.com/zoobc/zoobc-core/core/util"
+	"golang.org/x/crypto/sha3"
 )
 
 type (
 	// NodeAdminServiceInterface represents interface for NodeAdminService
 	NodeAdminServiceInterface interface {
+		GenerateProofOfOwnership(accountType string, accountAddress uint32, signature []byte)
+		ValidateProofOfOwnership()
 	}
 
-	// NodeAdminService contains all transactions in mempool plus a mux to manage locks in concurrency
 	NodeAdminService struct {
 		QueryExecutor query.ExecutorInterface
 		BlockQuery    query.BlockQueryInterface
 		AccountQuery  query.AccountQueryInterface
-		Sign          crypto.SignatureInterface
+		Signature     crypto.SignatureInterface
 	}
 )
 
 // generate proof of ownership
-func (nas *NodeAdminService) GenerateProofOfOwnership() error {
-	nas.LookupLastBlock()
-	nas.LookupOwnerAccount()
-	nas.SignData()
+func (nas *NodeAdminService) GenerateProofOfOwnership(accountType uint32, accountAddress string, signature []byte) (nodeMessages []byte, proofOfOwnershipSign []byte) {
+
+	lastBlock, lastBlockHash, _ := nas.LookupLastBlock()
+
+	ownerAccount, _ := nas.LookupOwnerAccount()
+
+	buffer := bytes.NewBuffer([]byte{})
+	buffer.Write(commonUtil.ConvertUint32ToBytes(accountType)[:2])
+	buffer.Write([]byte(accountAddress))
+	buffer.Write(lastBlockHash)
+	buffer.Write(commonUtil.ConvertUint32ToBytes(lastBlock.Height))
+
+	if ownerAccount.AccountType == accountType && ownerAccount.Address == accountAddress {
+		nodeMessages = buffer.Bytes()
+		proofOfOwnershipSign = nas.SignData(nodeMessages)
+
+		return nodeMessages, proofOfOwnershipSign
+	}
 }
 
 // GetLastBlock return the last pushed block
-func (nas *NodeAdminService) LookupLastBlock() (*model.Block, error) {
+func (nas *NodeAdminService) LookupLastBlock() (*model.Block, []byte, error) {
 	rows, err := nas.QueryExecutor.ExecuteSelect(nas.BlockQuery.GetLastBlock())
 	defer func() {
 		if rows != nil {
@@ -37,50 +58,42 @@ func (nas *NodeAdminService) LookupLastBlock() (*model.Block, error) {
 		}
 	}()
 	if err != nil {
-		return &model.Block{
+		return nil, &model.Block{
 			ID: -1,
 		}, err
 	}
 	var blocks []*model.Block
 	blocks = nas.BlockQuery.BuildModel(blocks, rows)
 	if len(blocks) > 0 {
-		return blocks[0], nil
+
+		digest := sha3.New512()
+		blockByte, _ := util.GetBlockByte(blocks[0], true)
+		_, _ = digest.Write(blockByte)
+		hash := digest.Sum([]byte{})
+
+		return blocks[0], hash, nil
 	}
-	return &model.Block{
+	return nil, &model.Block{
 		ID: -1,
 	}, errors.New("BlockNotFound")
 
 }
 
-func (nas *NodeAdminService) LookupOwnerAccount(accountID []byte) (*model.Account, error){
-	rows, err := nas.QueryExecutor.ExecuteSelect(nas.AccountQuery.GetAccountByID(accountID []byte))
-	defer func() {
-		if rows != nil {
-			_ = rows.Close()
-		}
-	}()
-	if err != nil {
-		return &model.Account{
-			ID: -1,
-		}, err
-	}
-	var account []*model.Account
-	account = nas.AccountQuery.BuildModel(account, rows)
-	if len(account) > 0 {
-		return account[0], nil
-	}
-	return &model.Account{
-		ID: -1,
-	}, errors.New("Account Not Found")
+func (nas *NodeAdminService) LookupOwnerAccount() (*model.Account, error) {
+	ownerAccountAddress := viper.GetString("ownerAccountAddress")
 }
 
-func (nas *NodeAdminService) SignData() error {
-	
+func (nas *NodeAdminService) SignData(payload []byte) (sign []byte) {
+	nodeSecretPhrase := viper.GetString("nodeSecretPhrase")
+	sign = nas.Signature.SignBlock(payload, nodeSecretPhrase)
+	return sign
 }
 
 // validate proof of ownership
 func (nas *NodeAdminService) ValidateProofOfOwnership(mpTx *model.MempoolTransaction) error {
-
+	nas.ValidateSignature()
+	nas.ValidateHeight()
+	nas.LookupBlock()
 }
 func (nas *NodeAdminService) ValidateSignature() error {
 
