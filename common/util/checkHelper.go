30bd9e14f532fcdb00256831391565d897ef37ed
capt4ce <ahmad.ali.abdilah@gmail.com>
2019-07-26 17:21:01 +0800

    performing change requested

--- common/util/checkHelper.go
+++ common/util/checkHelper.go
@@ -6,11 +6,11 @@ import (
 )
 
 /*
-ValidateIP4 to
+ValidateIP4 validates format of ipv4
 */
 func ValidateIP4(ipAddress string) bool {
 	ipAddress = strings.Trim(ipAddress, " ")
-	pettern := `^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`
-	re, _ := regexp.Compile(pettern)
+	pattern := `^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`
+	re, _ := regexp.Compile(pattern)
 	return re.MatchString(ipAddress)
 }
