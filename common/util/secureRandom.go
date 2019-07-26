217541883c2bfe0282f1e0cc9b849e7e8dca929d
sukrawidhyawan <g.sukrawidhyawan02@gmail.com>
2019-07-26 17:52:39 +0800

    update func GetSecureRandom

--- common/util/secureRandom.go
+++ common/util/secureRandom.go
@@ -1,11 +1,15 @@
 package util
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"math"
+	"math/big"
 )
 
 // GetSecureRandom generates a int64 secure random
 // TODO: implement the real function to generate a secure random. for now we generate a pseudo-secure number
 func GetSecureRandom() int64 {
-	return rand.Int63()
+	max := *big.NewInt(math.MaxInt64)
+	newNumber, _ := rand.Int(rand.Reader, &max)
+	return newNumber.Int64()
 }
