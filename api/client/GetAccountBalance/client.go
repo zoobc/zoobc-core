5ced901294dce1b284fe48b53498fca6ff30486d
andy-shi88 <shi88.andy@gmail.com>
2019-08-02 17:33:30 +0800

    #112 add handler for account balance

---
+++ api/client/GetAccountBalance/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+
+	log "github.com/sirupsen/logrus"
+	rpc_model "github.com/zoobc/zoobc-core/common/model"
+	rpc_service "github.com/zoobc/zoobc-core/common/service"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial(":3001", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := rpc_service.NewAccountBalanceServiceClient(conn)
+
+	response, err := c.GetAccountBalance(context.Background(), &rpc_model.GetAccountBalanceRequest{
+		AccountType:    0,
+		AccountAddress: "BCZnSfqpP5tqFQlMTYkDeBVFWnbyVK7vLr5ORFpTjgtN",
+	})
+
+	if err != nil {
+		log.Fatalf("error calling rpc_service.GetAccountBalance: %s", err)
+	}
+
+	log.Printf("response from remote rpc_service.GetBlockByID(): %s", response)
+
+}
---
+++ api/handler/accountBalanceHandler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/zoobc/zoobc-core/api/service"
+	"github.com/zoobc/zoobc-core/common/model"
+)
+
+type AccountBalanceHandler struct {
+	Service service.AccountBalanceServiceInterface
+}
+
+func (abh *AccountBalanceHandler) GetAccountBalance(ctx context.Context, request *model.GetAccountBalanceRequest) (*model.GetAccountBalanceResponse, error) {
+	accountBalance, err := abh.Service.GetAccountBalance(request)
+	if err != nil {
+		return nil, err
+	}
+	return accountBalance, nil
+}
+
+func (abh *AccountBalanceHandler) GetAccountBalances(ctx context.Context, request *model.GetAccountBalancesRequest) (*model.GetAccountBalancesResponse, error) {
+	return nil, nil
+}
