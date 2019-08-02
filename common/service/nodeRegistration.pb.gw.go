61ee67c6d45897122aa427c2cf4fb2f0f6be14fe
stefano galassi <stefano.galassi@kyepot.com>
2019-08-02 11:53:47 +0800

    recompiled schema: added updatenoderegistration request and rpc endpoint to schema

--- common/service/nodeRegistration.pb.gw.go
+++ common/service/nodeRegistration.pb.gw.go
@@ -69,6 +69,26 @@ func request_NodeRegistrationService_GetNodeRegistration_0(ctx context.Context, 
 
 }
 
+var (
+	filter_NodeRegistrationService_UpdateNodeRegistration_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_NodeRegistrationService_UpdateNodeRegistration_0(ctx context.Context, marshaler runtime.Marshaler, client NodeRegistrationServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq model.UpdateNodeRegistrationRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_NodeRegistrationService_UpdateNodeRegistration_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.UpdateNodeRegistration(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterNodeRegistrationServiceHandlerFromEndpoint is same as RegisterNodeRegistrationServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterNodeRegistrationServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -147,6 +167,26 @@ func RegisterNodeRegistrationServiceHandlerClient(ctx context.Context, mux *runt
 
 	})
 
+	mux.Handle("GET", pattern_NodeRegistrationService_UpdateNodeRegistration_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_NodeRegistrationService_UpdateNodeRegistration_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_NodeRegistrationService_UpdateNodeRegistration_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -154,10 +194,14 @@ var (
 	pattern_NodeRegistrationService_GetNodeRegistrations_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "nodeRegistration", "GetNodeRegistrations"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_NodeRegistrationService_GetNodeRegistration_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "nodeRegistration", "GetNodeRegistration"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_NodeRegistrationService_UpdateNodeRegistration_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "nodeRegistration", "GetNodeRegistration"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
 	forward_NodeRegistrationService_GetNodeRegistrations_0 = runtime.ForwardResponseMessage
 
 	forward_NodeRegistrationService_GetNodeRegistration_0 = runtime.ForwardResponseMessage
+
+	forward_NodeRegistrationService_UpdateNodeRegistration_0 = runtime.ForwardResponseMessage
 )
