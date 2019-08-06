b406eb0bd026a22adf307bb4bd34303bb741ed32
stefano galassi <stefano.galassi@kyepot.com>
2019-08-06 16:39:14 +0800

    refactored common/util get/parse transaction bytes in respect to new standards started refactoring unit tests

---
+++ common/util/helper.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"bytes"
+	"errors"
+)
+
+func ReadTransactionBytes(buf *bytes.Buffer, nBytes int) ([]byte, error) {
+	nextBytes := buf.Next(nBytes)
+	if len(nextBytes) < nBytes {
+		return nil, errors.New("EndOfBufferReached")
+	}
+	return nextBytes, nil
+}
