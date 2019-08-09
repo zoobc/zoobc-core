41b68bef0a6861dc3250c313d52f00413bd37751
Ariasa Ketut <ariasta.ketut@icloud.com>
2019-08-09 11:36:49 +0800

    113 get transactions by account address (#129)
    * proto file build
    
    * create new query interface that would be dynamic query builder
    
    * api getTransactions include accountType and accountAddress
    
    * remove accountType from GetTransactionsRequest
    
    * fix description
    
    * remove accountType from request

--- api/client/GetTransactions/client.go
+++ api/client/GetTransactions/client.go
@@ -4,21 +4,23 @@ import (
 	"context"
 
 	log "github.com/sirupsen/logrus"
-	rpc_model "github.com/zoobc/zoobc-core/common/model"
-	rpc_service "github.com/zoobc/zoobc-core/common/service"
+	rpcModel "github.com/zoobc/zoobc-core/common/model"
+	rpcService "github.com/zoobc/zoobc-core/common/service"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	conn, err := grpc.Dial(":8000", grpc.WithInsecure())
+	conn, err := grpc.Dial(":3001", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %s", err)
 	}
 	defer conn.Close()
 
-	c := rpc_service.NewTransactionServiceClient(conn)
+	c := rpcService.NewTransactionServiceClient(conn)
 
-	response, err := c.GetTransactions(context.Background(), &rpc_model.GetTransactionsRequest{})
+	response, err := c.GetTransactions(context.Background(), &rpcModel.GetTransactionsRequest{
+		AccountAddress: "BCZD_VxfO2S9aziIL3cn_cXW7uPDVPOrnXuP98GEAUC7",
+	})
 
 	if err != nil {
 		log.Fatalf("error calling rpc_service.GetTransactions: %s", err)
