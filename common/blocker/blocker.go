ad6834888977e25d127898ccd443cd522f2b0b1e
Ariasa Ketut <ariasta.ketut@icloud.com>
2019-08-14 16:39:57 +0800

    114 get unconfirmed transactions (#144)
    * implements unconfirmed transactions and modified transaction services

---
+++ common/blocker/blocker.go
@@ -0,0 +1,19 @@
+package blocker
+
+type (
+	TypeBlocker int
+
+	Blocker struct {
+		Type    TypeBlocker
+		Message string
+	}
+)
+
+var (
+	DBErr    TypeBlocker = 1
+	BlockErr TypeBlocker = 2
+)
+
+func (e Blocker) Error() string {
+	return e.Message
+}
