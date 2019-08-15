20d799ef09bb909e50c9912b5e5e9bce8bbd7f7d
andy-shi88 <shi88.andy@gmail.com>
2019-08-15 11:02:44 +0800

    #136 remove length

--- common/transaction/sendMoney.go
+++ common/transaction/sendMoney.go
@@ -15,7 +15,6 @@ import (
 type SendMoney struct {
 	Body                          *model.SendMoneyTransactionBody
 	Fee                           int64
-	SenderAccountAddressLength    uint32
 	SenderAddress                 string
 	RecipientAccountAddressLength uint32
 	RecipientAddress              string
