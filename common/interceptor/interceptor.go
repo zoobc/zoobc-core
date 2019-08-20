a06215dca14499696c047e6740929a113d9d8d2c
andy-shi88 <shi88.andy@gmail.com>
2019-08-20 16:09:31 +0800

    #159 move interceptor package

---
+++ common/interceptor/interceptor.go
@@ -0,0 +1,171 @@
+package interceptor
+
+import (
+	"context"
+	"fmt"
+	"github.com/zoobc/zoobc-core/common/blocker"
+	"github.com/zoobc/zoobc-core/common/crypto"
+	"github.com/zoobc/zoobc-core/common/model"
+	"google.golang.org/grpc/metadata"
+	"runtime"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"google.golang.org/grpc"
+)
+
+/*
+NewServerInterceptor function can use to inject middlewares like:
+	- `recover`
+	- `log` triggered
+	- validate `authentication` if needed
+With `recover()` function can handle re-run the app while got panic or error
+*/
+func NewServerInterceptor(logger *logrus.Logger) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		var (
+			errHandler error
+			resp       interface{}
+		)
+		start := time.Now()
+		fields := logrus.Fields{
+			"method": info.FullMethod,
+			"time":   start.String(),
+		}
+
+		defer func() {
+
+			fields["latency"] = fmt.Sprintf("%d ns", time.Since(start).Nanoseconds())
+			err := recover()
+			if err != nil {
+				// get stack after panic called and perhaps its first error
+				_, file, line, _ := runtime.Caller(4)
+				fields["panic"] = fmt.Sprintf("%s %d", file, line)
+			} else if errHandler != nil {
+				fields["error"] = errHandler
+			}
+
+			if logger != nil {
+				switch {
+				case err != nil:
+					logger.WithFields(fields).Error(fmt.Sprint(err))
+				case errHandler != nil:
+					logger.WithFields(fields).Warning(errHandler)
+				default:
+					logger.WithFields(fields).Info("success")
+				}
+			}
+		}()
+
+		resp, errHandler = handler(ctx, req)
+		return resp, errHandler
+	}
+}
+
+/*
+NewClientInterceptor function can use to inject using grpc client like:
+	- `recover`
+	- `log` triggered
+	- add access token if needed
+With `recover()` function can handle re-run the app while got panic
+*/
+func NewClientInterceptor(logger *logrus.Logger) grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req interface{},
+		reply interface{},
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		var (
+			errInvoker error
+		)
+		start := time.Now()
+		fields := logrus.Fields{
+			"method": method,
+			"time":   start.String(),
+		}
+
+		defer func() {
+			fields["latency"] = fmt.Sprintf("%d ns", time.Since(start).Nanoseconds())
+			err := recover()
+			if err != nil {
+				// get stack after panic called and perhaps its first error
+				_, file, line, _ := runtime.Caller(4)
+				fields["panic"] = fmt.Sprintf("%s %d", file, line)
+			}
+			if logger != nil {
+				switch {
+				case err != nil:
+					logger.WithFields(fields).Error(fmt.Sprint(err))
+				case errInvoker != nil:
+					logger.WithFields(fields).Warning(fmt.Sprint(errInvoker))
+				default:
+					logger.WithFields(fields).Info("success")
+				}
+			}
+		}()
+		errInvoker = invoker(ctx, method, req, reply, cc, opts...)
+		return errInvoker
+	}
+
+}
+
+/*
+NewNodeAdminAuthStreamInterceptor
+validate request against the destination service and the signature with the node owner
+*/
+func NewNodeAdminAuthStreamInterceptor(ownerAddress string) grpc.StreamServerInterceptor {
+	return func(
+		srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler,
+	) error {
+		var (
+			errHandler  error
+			requestType model.RequestType
+		)
+		switch info.FullMethod {
+		case "/service.NodeHardwareService/GetNodeHardware":
+			requestType = model.RequestType_GetNodeHardware
+		default:
+			// unprotected service, by pass the auth checking
+			requestType = -1
+		}
+		md, ok := metadata.FromIncomingContext(ss.Context())
+		if !ok {
+			return blocker.NewBlocker(
+				blocker.AuthErr,
+				"metadata not provided",
+			)
+		}
+		authSlice := md.Get("authorization")
+		if authSlice == nil {
+			return blocker.NewBlocker(
+				blocker.AuthErr,
+				"authorization metadata not provided",
+			)
+		}
+		if requestType > -1 {
+			// validate request
+			// todo: this is verifying against whatever owner address is in the config file, update this
+			// todo: to follow how `claim` node work.
+			err := crypto.VerifyAuthAPI(
+				ownerAddress,
+				authSlice[0],
+				requestType)
+			if err != nil {
+				return err
+			}
+		}
+
+		errHandler = handler(srv, ss)
+		return errHandler
+	}
+}
---
+++ common/interceptor/interceptor_test.go
@@ -0,0 +1,323 @@
+package interceptor
+
+import (
+	"bytes"
+	"context"
+	"encoding/hex"
+	"github.com/google/go-cmp/cmp"
+	"github.com/sirupsen/logrus"
+	"github.com/zoobc/zoobc-core/common/constant"
+	"github.com/zoobc/zoobc-core/common/crypto"
+	rpcModel "github.com/zoobc/zoobc-core/common/model"
+	"github.com/zoobc/zoobc-core/common/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"testing"
+	"time"
+)
+
+var (
+	mockOwnerAddress = "BCZEGOb3WNx3fDOVf9ZS4EjvOIv_UeW4TVBQJ_6tHKlE"
+	mockOwnerSeed    = "concur vocalist rotten busload gap quote stinging undiluted surfer goofiness deviation starved"
+)
+
+type (
+	mockServerStreamSuccess struct{}
+	mockServerStreamNoAuth  struct {
+		mockServerStreamSuccess
+	}
+	mockServerStreamNoMetadata struct {
+		mockServerStreamSuccess
+	}
+	mockServerStreamInvalidAuth struct {
+		mockServerStreamSuccess
+	}
+)
+
+func (*mockServerStreamSuccess) SetHeader(metadata.MD) error  { return nil }
+func (*mockServerStreamSuccess) SendHeader(metadata.MD) error { return nil }
+func (*mockServerStreamSuccess) SetTrailer(metadata.MD)       {}
+func (*mockServerStreamSuccess) Context() context.Context {
+	currentTime := uint64(time.Now().Unix())
+	buffer := bytes.NewBuffer([]byte{})
+	buffer.Write(util.ConvertUint32ToBytes(uint32(rpcModel.RequestType_GetNodeHardware)))
+	buffer.Write(util.ConvertUint64ToBytes(currentTime))
+	sig := (crypto.NewSignature()).Sign(
+		buffer.Bytes(),
+		constant.NodeSignatureTypeDefault,
+		mockOwnerSeed,
+	)
+	buffer.Write(sig)
+	ctx := context.Background()
+	md := metadata.Pairs("authorization", hex.EncodeToString(buffer.Bytes()))
+	ctx = metadata.NewIncomingContext(ctx, md)
+	return ctx
+}
+func (*mockServerStreamSuccess) SendMsg(m interface{}) error { return nil }
+func (*mockServerStreamSuccess) RecvMsg(m interface{}) error { return nil }
+
+func (*mockServerStreamNoAuth) Context() context.Context {
+	ctx := context.Background()
+	md := metadata.Pairs("foo", "bar")
+	ctx = metadata.NewIncomingContext(ctx, md)
+	return ctx
+}
+
+func (*mockServerStreamNoMetadata) Context() context.Context {
+	ctx := context.Background()
+	return ctx
+}
+
+func (*mockServerStreamInvalidAuth) Context() context.Context {
+	// sign with invalid request type parameter
+	currentTime := uint64(time.Now().Unix())
+	buffer := bytes.NewBuffer([]byte{})
+	buffer.Write(util.ConvertUint32ToBytes(uint32(1435)))
+	buffer.Write(util.ConvertUint64ToBytes(currentTime))
+	sig := (crypto.NewSignature()).Sign(
+		buffer.Bytes(),
+		constant.NodeSignatureTypeDefault,
+		mockOwnerSeed,
+	)
+	buffer.Write(sig)
+	ctx := context.Background()
+	md := metadata.Pairs("authorization", hex.EncodeToString(buffer.Bytes()))
+	ctx = metadata.NewIncomingContext(ctx, md)
+	return ctx
+}
+
+func TestNewServerInterceptor(t *testing.T) {
+	type args struct {
+		logger *logrus.Logger
+	}
+	tests := []struct {
+		name        string
+		args        args
+		want        grpc.UnaryServerInterceptor
+		wantRecover bool
+	}{
+		{
+			name: "wantRecover",
+			args: args{
+				logger: logrus.New(),
+			},
+			want: func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+				return nil, status.Errorf(codes.Internal, "there's something wrong")
+			},
+			wantRecover: true,
+		},
+		{
+			name: "wantNotRecover",
+			args: args{
+				logger: logrus.New(),
+			},
+			want: func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+				return nil, status.Errorf(codes.Internal, "there's something wrong")
+			},
+			wantRecover: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewServerInterceptor(tt.args.logger)
+			if cmp.Equal(got, tt.want) {
+				t.Errorf("NewInterceptor() = %v, want %v", got, tt.want)
+			}
+			testServerInterceptor(got, tt.wantRecover)
+		})
+	}
+}
+
+func testServerInterceptor(fn grpc.UnaryServerInterceptor, wantRecover bool) {
+	var (
+		handler grpc.UnaryHandler
+	)
+	if wantRecover {
+		handler = func(ctx context.Context, req interface{}) (resp interface{}, err error) {
+			panic(handler)
+		}
+	} else {
+		handler = func(ctx context.Context, req interface{}) (resp interface{}, err error) {
+			return nil, status.Errorf(codes.Internal, "there's something wrong")
+		}
+	}
+	_, _ = fn(context.Background(), nil, &grpc.UnaryServerInfo{}, handler)
+}
+
+func TestNewClientInterceptor(t *testing.T) {
+	type args struct {
+		logger *logrus.Logger
+	}
+	tests := []struct {
+		name        string
+		args        args
+		want        grpc.UnaryClientInterceptor
+		wantRecover bool
+	}{
+		{
+			name: "wantRecover",
+			args: args{
+				logger: logrus.New(),
+			},
+			want: func(
+				ctx context.Context,
+				method string,
+				req, reply interface{},
+				cc *grpc.ClientConn,
+				invoker grpc.UnaryInvoker,
+				opts ...grpc.CallOption) error {
+				return status.Errorf(codes.Internal, "there's something wrong")
+			},
+			wantRecover: false,
+		},
+		{
+			name: "wantRecover",
+			args: args{
+				logger: logrus.New(),
+			},
+			want: func(
+				ctx context.Context,
+				method string,
+				req, reply interface{},
+				cc *grpc.ClientConn,
+				invoker grpc.UnaryInvoker,
+				opts ...grpc.CallOption) error {
+				return status.Errorf(codes.Internal, "there's something wrong")
+			},
+			wantRecover: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewClientInterceptor(tt.args.logger)
+			if cmp.Equal(got, tt.want) {
+				t.Errorf("NewClientInterceptor() = %v, want %v", got, tt.want)
+			}
+			testClientInterceptor(got, tt.wantRecover)
+		})
+	}
+}
+
+func testClientInterceptor(fn grpc.UnaryClientInterceptor, wantRecover bool) {
+	var (
+		invoker grpc.UnaryInvoker
+	)
+	if wantRecover {
+		invoker = func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
+			panic(invoker)
+		}
+	} else {
+		invoker = func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
+			return status.Errorf(codes.Internal, "there's something wrong")
+		}
+	}
+
+	cc, _ := grpc.Dial("127.0.0.1:8001", grpc.WithInsecure())
+	_ = fn(context.Background(), "testMethod", nil, nil, cc, invoker, nil)
+
+}
+
+func TestNewNodeAdminAuthStreamInterceptor(t *testing.T) {
+
+	type args struct {
+		ownerAddress string
+		fullMethod   string
+		handler      grpc.StreamHandler
+		serverStream grpc.ServerStream
+	}
+	tests := []struct {
+		name         string
+		args         args
+		want         grpc.StreamServerInterceptor
+		wantInnerErr bool
+	}{
+		{
+			name: "wantErr:noMetadata",
+			args: args{
+				ownerAddress: mockOwnerAddress,
+				fullMethod:   "/service.NodeHardwareService/GetNodeHardware",
+				serverStream: &mockServerStreamNoMetadata{},
+			},
+			want: func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
+				return nil
+			},
+			wantInnerErr: true,
+		},
+		{
+			name: "wantErr:noAuth",
+			args: args{
+				ownerAddress: mockOwnerAddress,
+				fullMethod:   "/service.NodeHardwareService/GetNodeHardware",
+				serverStream: &mockServerStreamNoAuth{},
+			},
+			want: func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
+				return nil
+			},
+			wantInnerErr: true,
+		},
+		{
+			name: "wantSuccess:GetNodeHardware",
+			args: args{
+				ownerAddress: mockOwnerAddress,
+				fullMethod:   "/service.NodeHardwareService/GetNodeHardware",
+				serverStream: &mockServerStreamSuccess{},
+				handler: func(srv interface{}, stream grpc.ServerStream) error {
+					return nil
+				},
+			},
+			want: func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
+				return nil
+			},
+			wantInnerErr: false,
+		},
+		{
+			name: "wantFail:invalidSignature",
+			args: args{
+				ownerAddress: mockOwnerAddress,
+				fullMethod:   "/service.NodeHardwareService/GetNodeHardware",
+				serverStream: &mockServerStreamInvalidAuth{},
+				handler: func(srv interface{}, stream grpc.ServerStream) error {
+					return nil
+				},
+			},
+			want: func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
+				return nil
+			},
+			wantInnerErr: true,
+		},
+		{
+			name: "wantSuccess:unprotectedService",
+			args: args{
+				ownerAddress: mockOwnerAddress,
+				fullMethod:   "/nonProtectedService",
+				serverStream: &mockServerStreamSuccess{},
+				handler: func(srv interface{}, stream grpc.ServerStream) error {
+					return nil
+				},
+			},
+			want: func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
+				return nil
+			},
+			wantInnerErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewNodeAdminAuthStreamInterceptor(tt.args.ownerAddress)
+			if cmp.Equal(got, tt.want) {
+				t.Errorf("NewInterceptor() = %v, want %v", got, tt.want)
+			}
+			info := &grpc.StreamServerInfo{
+				FullMethod:     tt.args.fullMethod,
+				IsClientStream: true,
+				IsServerStream: true,
+			}
+			err := got(nil, tt.args.serverStream, info, tt.args.handler)
+			if err != nil && tt.wantInnerErr != true {
+				t.Error("unexpected error occurred")
+			}
+		})
+	}
+}
