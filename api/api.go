45bee289320ecb557daae1a4330aa2792084fd23
andy-shi88 <shi88.andy@gmail.com>
2019-08-21 13:21:52 +0800

    #170 add flag for activating and disabling http proxy

--- api/api.go
+++ api/api.go
@@ -109,9 +109,11 @@ func startGrpcServer(port int, queryExecutor query.ExecutorInterface, p2pHostSer
 func Start(grpcPort, restPort int, queryExecutor query.ExecutorInterface, p2pHostService p2p.ServiceInterface,
 	blockServices map[int32]coreService.BlockServiceInterface, ownerAccountAddress string) {
 	startGrpcServer(grpcPort, queryExecutor, p2pHostService, blockServices, ownerAccountAddress)
-	go func() {
-		_ = runProxy(restPort, grpcPort)
-	}()
+	if restPort > 0 { // only start proxy service if apiHTTPPort set with value > 0
+		go func() {
+			_ = runProxy(restPort, grpcPort)
+		}()
+	}
 }
 
 /**
