8a84886b27ce95fbaeeb51de3a627bbb20548b5f
Andy Shi <shi88.andy@gmail.com>
2019-08-23 10:31:01 +0800

    176 transaction order (#179)
    * #176 add transaction index
    * #176 save transaction index to transaction table on transaction applied

--- common/query/transactionQuery.go
+++ common/query/transactionQuery.go
@@ -43,6 +43,7 @@ func NewTransactionQuery(chaintype chaintype.ChainType) *TransactionQuery {
 			"transaction_body_bytes",
 			"signature",
 			"version",
+			"transaction_index",
 		},
 		TableName: "\"transaction\"",
 		ChainType: chaintype,
@@ -111,7 +112,8 @@ func (*TransactionQuery) ExtractModel(tx *model.Transaction) []interface{} {
 		&tx.TransactionBodyLength,
 		&tx.TransactionBodyBytes,
 		&tx.Signature,
-		tx.Version,
+		&tx.Version,
+		&tx.TransactionIndex,
 	}
 }
 
@@ -132,6 +134,7 @@ func (*TransactionQuery) BuildModel(txs []*model.Transaction, rows *sql.Rows) []
 			&tx.TransactionBodyBytes,
 			&tx.Signature,
 			&tx.Version,
+			&tx.TransactionIndex,
 		)
 		txs = append(txs, &tx)
 	}
