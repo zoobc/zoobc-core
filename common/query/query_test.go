9d1d0cef1d0633b667fedbfb2b46cfe42f43221e
sukrawidhyawan <g.sukrawidhyawan02@gmail.com>
2019-08-28 18:12:30 +0800

    update test case GetDerivedQuery

--- common/query/query_test.go
+++ common/query/query_test.go
@@ -24,6 +24,7 @@ func TestGetDerivedQuery(t *testing.T) {
 				NewTransactionQuery(chaintype.GetChainType(0)),
 				NewNodeRegistrationQuery(),
 				NewAccountBalanceQuery(),
+				NewAccountDatasetsQuery(),
 			},
 		},
 	}
