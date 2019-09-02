5770aeb62e0078781cee572d5b9da54d93f18845
andy-shi88 <shi88.andy@gmail.com>
2019-09-02 10:51:46 +0800

    #200 fix get node hardware stream, client only need to send auth once now

--- api/client/GetNodeHardware/client.go
+++ api/client/GetNodeHardware/client.go
@@ -41,32 +41,31 @@ func main() {
 	}
 	waitC := make(chan struct{})
 	signature := crypto.Signature{}
+	currentTime := uint64(time.Now().Unix())
+	buffer := bytes.NewBuffer([]byte{})
+	buffer.Write(util.ConvertUint64ToBytes(currentTime))
+	buffer.Write(util.ConvertUint32ToBytes(uint32(rpcModel.RequestType_GetNodeHardware)))
+	sig := signature.Sign(
+		buffer.Bytes(),
+		constant.NodeSignatureTypeDefault,
+		"concur vocalist rotten busload gap quote stinging undiluted surfer goofiness deviation starved",
+	)
+	buffer.Write(sig)
 
+	ctx := context.Background()
+
+	md := metadata.Pairs("authorization", base64.StdEncoding.EncodeToString(buffer.Bytes()))
+	ctx = metadata.NewOutgoingContext(ctx, md)
+	stream, err = c.GetNodeHardware(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = stream.Send(&rpcModel.GetNodeHardwareRequest{})
+	if err != nil {
+		log.Fatalf("error sending request to rpcService.GetNodeHardware: %s", err)
+	}
 	go func() {
 		for {
-			currentTime := uint64(time.Now().Unix())
-			buffer := bytes.NewBuffer([]byte{})
-			buffer.Write(util.ConvertUint64ToBytes(currentTime))
-			buffer.Write(util.ConvertUint32ToBytes(uint32(rpcModel.RequestType_GetNodeHardware)))
-			sig := signature.Sign(
-				buffer.Bytes(),
-				constant.NodeSignatureTypeDefault,
-				"concur vocalist rotten busload gap quote stinging undiluted surfer goofiness deviation starved",
-			)
-			buffer.Write(sig)
-
-			ctx := context.Background()
-
-			md := metadata.Pairs("authorization", base64.StdEncoding.EncodeToString(buffer.Bytes()))
-			ctx = metadata.NewOutgoingContext(ctx, md)
-			stream, err = c.GetNodeHardware(ctx)
-			log.Println("Sleeping...")
-			time.Sleep(2 * time.Second)
-			log.Println("Sending request...")
-			err = stream.Send(&rpcModel.GetNodeHardwareRequest{})
-			if err != nil {
-				log.Fatalf("error sending request to rpcService.GetNodeHardware: %s", err)
-			}
 			response, err := stream.Recv()
 			if err != nil {
 				log.Fatalf("error receiving response from rpcService.GetNodeHardware: %s", err)
