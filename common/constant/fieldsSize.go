01a6e3412830678dfcd23dd7e150e6e25c6f0104
Andy Shi <shi88.andy@gmail.com>
2019-09-23 16:11:08 +0800

    210 smithing validation (#242)
    * #210 order blocksmiths
    
    * #210 update hash 512 on block to 256
    
    * #210 restructure how blocksmith ordering work
    
    * #210 init observer before adding genesis
    
    * #210 pass sortedBlocksmith by the pointer of its slice so core, p2p and other submodule will be able to access the it
    
    * #210 fix test after changing block hash to sha256
    
    * #210 update tx receipt to use sha256
    
    * #210 update hash 512 on block to 256 and update genesis block
    
    * #210 add readme section for activating grpcwebproxy on server
    
    * #210 inject sortedBlocksmith to blockservice instead of p2p

--- common/constant/fieldsSize.go
+++ common/constant/fieldsSize.go
@@ -7,7 +7,7 @@ var (
 	// NodePublicKey TODO: this is valid for pub keys generated using Ed25519. in future we might have more implementations
 	NodePublicKey uint32 = 32
 	Balance       uint32 = 8
-	BlockHash     uint32 = 64
+	BlockHash     uint32 = 32
 	Height        uint32 = 4
 	// NodeSignature node always signs using Ed25519 algorithm, that produces 64 bytes signatures
 	NodeSignature         uint32 = 64
