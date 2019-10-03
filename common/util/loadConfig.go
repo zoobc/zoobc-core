06f2357bc4b06061b11238f798ef55df4b50cef8
komodo2662 <thomas.prabowo@blockchainzoo.com>
2019-10-03 17:16:25 +0800

    change loadConfig function

--- common/util/loadConfig.go
+++ common/util/loadConfig.go
@@ -9,19 +9,21 @@ import (
 /*
 LoadConfig must be called at first time while start the app
 */
-func LoadConfig(path, name, extension string) error {
+func LoadConfig(path, name string, debug bool, extension string) error {
 
 	if len(path) < 1 || len(name) < 1 || len(extension) < 1 {
 		return fmt.Errorf("path and extension cannot be nil")
 	}
 
-	viper.SetDefault("dbName", "spinechain.db")
-	viper.SetDefault("dbPath", "./resource")
-	viper.SetDefault("apiRPCPort", 8080)
-	viper.SetDefault("apiHTTPPort", 0)
-	viper.SetDefault("logLevels", []string{"error", "fatal", "panic"})
-	viper.SetConfigName(name)
-	viper.SetConfigType(extension)
+	if debug == false {
+		viper.SetDefault("dbName", "spinechain.db")
+		viper.SetDefault("dbPath", "./resource")
+		viper.SetDefault("apiRPCPort", 8080)
+		viper.SetDefault("apiHTTPPort", 0)
+		viper.SetDefault("logLevels", []string{"error", "fatal", "panic"})
+		viper.SetConfigName(name)
+		viper.SetConfigType(extension)
+	}
 	viper.AddConfigPath(path)
 	viper.AddConfigPath("../../resource")
 
