c16723969af46af56a3650e71969529151477dce
Andy Shi <shi88.andy@gmail.com>
2019-10-08 14:23:06 +0800

    307 transaction timestamp validation (#308)
    * #307 validate if transaction is coming from future timestamp in endpoint validation
    
    * #307 comment in the unit of a time constants
    
    * #307 fix tx timestamp validation

--- common/constant/transaction.go
+++ common/constant/transaction.go
@@ -4,7 +4,7 @@ var (
 	MaxNumberOfTransactions           = 255
 	MinTransactionSize                = 176
 	MaxPayloadLength                  = MinTransactionSize * MaxNumberOfTransactions
-	TransactionExpirationOffset int64 = 3600
+	TransactionExpirationOffset int64 = 3600 // 3600 seconds
 	TxFeePerByte                int32
 	SignatureTypeDefault        = uint32(0)
 )
