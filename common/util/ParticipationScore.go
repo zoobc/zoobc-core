5cf831422a0f47664231100327b32109b826a8c7
andy-shi88 <shi88.andy@gmail.com>
2019-10-17 10:52:58 +0800

    #340 modify calculate POP score utility to have dynamic maximum recipient count based on the active node

--- common/util/ParticipationScore.go
+++ common/util/ParticipationScore.go
@@ -6,8 +6,8 @@ import (
 )
 
 // CalculateParticipationScore to calculate score change of node
-func CalculateParticipationScore(linkedReceipt, unlinkedReceipt uint32) (int64, error) {
-	if (linkedReceipt + unlinkedReceipt) > constant.MaxReceipt {
+func CalculateParticipationScore(linkedReceipt, unlinkedReceipt, maxReceipt uint32) (int64, error) {
+	if (linkedReceipt + unlinkedReceipt) > maxReceipt {
 		return 0, blocker.NewBlocker(
 			blocker.ValidationErr,
 			"CalculateParticipationScore, the number of receipt exceeds",
@@ -15,11 +15,11 @@ func CalculateParticipationScore(linkedReceipt, unlinkedReceipt uint32) (int64, 
 	}
 
 	// Maximum score will get when create a block
-	maxBlockScore := int64(float32(constant.MaxReceipt) * constant.LinkedReceiptScore * constant.ScalarReceiptScore)
+	maxBlockScore := int64(float32(maxReceipt) * constant.LinkedReceiptScore * constant.ScalarReceiptScore)
 	halfMaxBlockScore := maxBlockScore / 2
 
-	linkedBlockScore := (float32(linkedReceipt) * constant.LinkedReceiptScore * constant.ScalarReceiptScore)
-	unlinkedBlockScore := (float32(unlinkedReceipt) * constant.UnlinkedReceiptScore * constant.ScalarReceiptScore)
+	linkedBlockScore := float32(linkedReceipt) * constant.LinkedReceiptScore * constant.ScalarReceiptScore
+	unlinkedBlockScore := float32(unlinkedReceipt) * constant.UnlinkedReceiptScore * constant.ScalarReceiptScore
 	blockScore := int64(linkedBlockScore + unlinkedBlockScore)
 
 	scoreChangeOfANode := ((blockScore - halfMaxBlockScore) * constant.MaxScoreChange) / halfMaxBlockScore
