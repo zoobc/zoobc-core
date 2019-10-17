491f438aef7d2363ed23338bded7b21e841451f7
sukrawidhyawan <g.sukrawidhyawan02@gmail.com>
2019-10-17 11:00:21 +0800

    fix  golangci, comment variabel

--- common/constant/sqlConnetion.go
+++ common/constant/sqlConnetion.go
@@ -1,8 +1,8 @@
 package constant
 
 var (
-	// Represent number of maximum idle connetion in sql pool connection
+	// SqlMaxConnectionLifetime Represent number of maximum idle connetion in sql pool connection
 	SqlMaxIdleConnections = 40
-	// Reprensent the expiration of idle connetion, the number in minutes
+	// SqlMaxConnectionLifetime Reprensent the expiration of idle connetion, the number in minutes
 	SqlMaxConnectionLifetime = 30
 )
