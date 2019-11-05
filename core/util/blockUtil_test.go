588d707236b4465f11c06cc4981585ef2dbfa606
iltoga <galaxy73.it@gmail.com>
2019-11-05 10:18:03 +0800

    399 test pseudorandom selection (#400)
    * added test to verify uniform smith distribution over time

--- core/util/blockUtil_test.go
+++ core/util/blockUtil_test.go
@@ -2,13 +2,18 @@
 package util
 
 import (
+	"encoding/csv"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"math/big"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
 
-	"github.com/zoobc/zoobc-core/common/query"
-
 	"github.com/zoobc/zoobc-core/common/model"
+	"github.com/zoobc/zoobc-core/common/query"
 )
 
 func TestGetBlockSeed(t *testing.T) {
@@ -327,6 +332,196 @@ func TestIsGenesis(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := IsGenesis(tt.args.previousBlockID, tt.args.block); got != tt.want {
 				t.Errorf("IsGenesis() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCalculateSmithOrder(t *testing.T) {
+	type args struct {
+		score     *big.Int
+		blockSeed *big.Int
+		nodeID    int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "CalculateSmithOrder:success",
+			args: args{
+				score:     new(big.Int).SetInt64(1000),
+				blockSeed: new(big.Int).SetInt64(1000),
+				nodeID:    int64(1000),
+			},
+			want: "7357219233906154824000",
+		},
+		{
+			name: "CalculateSmithOrder:success2",
+			args: args{
+				score:     new(big.Int).SetInt64(2000),
+				blockSeed: new(big.Int).SetInt64(3000),
+				nodeID:    int64(10),
+			},
+			want: "9845301605531286876000",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CalculateSmithOrder(tt.args.score, tt.args.blockSeed,
+				tt.args.nodeID); !reflect.DeepEqual(fmt.Sprintf("%d", got), tt.want) {
+				t.Errorf("CalculateSmithOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// TestCalculateSmithOrderDistribution simulation of pseudo-random smith distribution
+func TestCalculateSmithOrderDistribution(t *testing.T) {
+	type args struct {
+		initialDistribution map[int64]int
+		score               *big.Int
+		blockSeeds          []*big.Int
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[int64]int
+	}{
+		{
+			name: "CalculateSmithOrderDistribution:success",
+			args: args{
+				initialDistribution: map[int64]int{
+					7357219233906154824:  0,
+					9145301605531286876:  0,
+					-8566484392056561567: 0,
+					7937868735467829764:  0,
+					-243345789637905342:  0,
+				},
+				score:      new(big.Int).SetInt64(1000),
+				blockSeeds: getBlockSeedsFromFile("blockSeeds1000.csv"),
+			},
+			want: map[int64]int{
+				-8566484392056561567: 211,
+				-243345789637905342:  195,
+				7357219233906154824:  207,
+				7937868735467829764:  178,
+				9145301605531286876:  209,
+			},
+		},
+		{
+			name: "CalculateSmithOrderDistribution:success2",
+			args: args{
+				initialDistribution: map[int64]int{
+					7357219233906154824:  0,
+					9145301605531286876:  0,
+					-8566484392056561567: 0,
+					7937868735467829764:  0,
+					-243345789637905342:  0,
+				},
+				score:      new(big.Int).SetInt64(1000),
+				blockSeeds: getBlockSeedsFromFile("blockSeeds10000.csv"),
+			},
+			want: map[int64]int{
+				-8566484392056561567: 1944,
+				-243345789637905342:  2062,
+				7357219233906154824:  2006,
+				7937868735467829764:  1987,
+				9145301605531286876:  2001,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got map[int64]int
+			if got = testCalculateSmithDistribution(tt.args.initialDistribution, tt.args.score,
+				tt.args.blockSeeds); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CalculateSmithOrderDistribution() = %v, want %v", got, tt.want)
+			}
+			// verify that we get the same distribution if we re-calculate using same input
+			if gotVerify := testCalculateSmithDistribution(tt.args.initialDistribution, tt.args.score,
+				tt.args.blockSeeds); !reflect.DeepEqual(gotVerify, got) {
+				t.Errorf("CalculateSmithOrderDistribution():veriry = %v, want %v", gotVerify, got)
+			}
+		})
+	}
+}
+
+func getBlockSeedsFromFile(fileName string) (blockSeeds []*big.Int) {
+	b, err := ioutil.ReadFile(filepath.Join("testdata", fileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(strings.NewReader(string(b)))
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, rec := range records {
+		seed, _ := new(big.Int).SetString(rec[0], 10)
+		blockSeeds = append(blockSeeds, seed)
+	}
+	return blockSeeds
+}
+
+func testCalculateSmithDistribution(distribution map[int64]int, score *big.Int, blockSeeds []*big.Int) map[int64]int {
+	for i := 0; i < len(blockSeeds); i++ {
+		var maxSmithRndNum *big.Int
+		var selectedID int64
+		j := 0
+		for nodeID := range distribution {
+			smithRndNum := CalculateSmithOrder(score, blockSeeds[i], nodeID)
+			if j == 0 {
+				maxSmithRndNum = smithRndNum
+				selectedID = nodeID
+				j++
+				continue
+			} else if smithRndNum.Cmp(maxSmithRndNum) > 0 {
+				maxSmithRndNum = smithRndNum
+				selectedID = nodeID
+			}
+		}
+		distribution[selectedID]++
+	}
+	return distribution
+}
+
+func TestCalculateNodeOrder(t *testing.T) {
+	type args struct {
+		score     *big.Int
+		blockSeed *big.Int
+		nodeID    int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "CalculateNodeOrder:success",
+			args: args{
+				score:     new(big.Int).SetInt64(1000),
+				blockSeed: new(big.Int).SetInt64(1000),
+				nodeID:    int64(1000),
+			},
+			want: "7357219233906154",
+		},
+		{
+			name: "CalculateNodeOrder:success2",
+			args: args{
+				score:     new(big.Int).SetInt64(2000),
+				blockSeed: new(big.Int).SetInt64(3000),
+				nodeID:    int64(10),
+			},
+			want: "2461325401382821",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CalculateNodeOrder(tt.args.score, tt.args.blockSeed,
+				tt.args.nodeID); !reflect.DeepEqual(fmt.Sprintf("%d", got), tt.want) {
+				t.Errorf("CalculateNodeOrder() = %v, want %v", got, tt.want)
 			}
 		})
 	}
