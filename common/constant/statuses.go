f68886c5b4e0aeaea17531cb6b3fabf7c8d35e51
capt4ce <ahmad.ali.abdilah@gmail.com>
2019-11-19 15:15:18 +0800

    fix linting

--- common/constant/statuses.go
+++ common/constant/statuses.go
@@ -3,6 +3,6 @@ package constant
 var (
 	BlockchainStatusIdle            = 1
 	BlockchainStatusGeneratingBlock = 2
-	BlockchainStatusReceivingBlock = 3
+	BlockchainStatusReceivingBlock  = 3
 	BlockchainStatusSyncingBlock    = 4
 )
