9bd561bc5b04f220be5e04090de318c5e5ca5618
andy-shi88 <shi88.andy@gmail.com>
2019-11-25 17:07:58 +0800

    fix test

--- core/service/blockCoreService_test.go
+++ core/service/blockCoreService_test.go
@@ -881,7 +881,7 @@ func TestBlockService_VerifySeed(t *testing.T) {
 				},
 				timestamp: 2,
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "VerifySeed:true-{elapsedTime>300 && seed < target ",
