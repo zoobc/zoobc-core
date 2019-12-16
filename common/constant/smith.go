99a5cc1a6660c18ef6d20a4680e8106ce0190e08
andy-shi88 <shi88.andy@gmail.com>
2019-12-16 09:22:54 +0800

    #525 remove unused constant

--- common/constant/smith.go
+++ common/constant/smith.go
@@ -1,16 +1,8 @@
 package constant
 
-import "math"
-
 var (
-	Two64                 = "18446744073709551616"
-	MaximumBalance        = int64(10000000000)
-	MaximumBlocktimeLimit = int64(67)
-	MinimumBlocktimeLimit = int64(53)
-	// AverageSmithingBlockHeight todo: inspect this number, this is for test only
-	AverageSmithingBlockHeight = uint32(math.MaxInt32)
-	MaxNumBlocksmithRewards    = int(5)
-	GenerateBlockTimeoutSec    = int64(15)
+	MaxNumBlocksmithRewards = 5
+	GenerateBlockTimeoutSec = int64(15)
 	// SmithingStartTime smithing initial delay from last block
 	SmithingStartTime           = int64(15) // second
 	SmithingBlockCreationTime   = int64(30)
