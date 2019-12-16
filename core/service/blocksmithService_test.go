fecedf66d587d85c7b4b2f3f41b6c94f37909d53
andy-shi88 <shi88.andy@gmail.com>
2019-12-16 09:37:34 +0800

    #525 fix test

--- core/service/blocksmithService_test.go
+++ core/service/blocksmithService_test.go
@@ -191,7 +191,7 @@ func TestBlocksmithService_GetSortedBlocksmiths(t *testing.T) {
 		want   []*model.Blocksmith
 	}{
 		{
-			name: "success",
+			name: "success : last sorted = incoming block height",
 			fields: fields{
 				QueryExecutor:            nil,
 				NodeRegistrationQuery:    nil,
@@ -210,10 +210,11 @@ func TestBlocksmithService_GetSortedBlocksmiths(t *testing.T) {
 				NodeRegistrationQuery:    tt.fields.NodeRegistrationQuery,
 				Logger:                   tt.fields.Logger,
 				SortedBlocksmiths:        tt.fields.SortedBlocksmiths,
+				LastSortedBlockHeight:    1,
 				SortedBlocksmithsMap:     tt.fields.SortedBlocksmithsMap,
 				SortedBlocksmithsMapLock: tt.fields.SortedBlocksmithsMapLock,
 			}
-			if got := bss.GetSortedBlocksmiths(&model.Block{Height: 0}); !reflect.DeepEqual(got, tt.want) {
+			if got := bss.GetSortedBlocksmiths(&model.Block{Height: 1}); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetSortedBlocksmiths() = %v, want %v", got, tt.want)
 			}
 		})
