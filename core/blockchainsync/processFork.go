6bdb24fef40d4dbae26f3054f6f831b7d8ee241d
andy-shi88 <shi88.andy@gmail.com>
2020-02-04 10:40:14 +0800

    handle shadowed error on db rollback

--- core/blockchainsync/processFork.go
+++ core/blockchainsync/processFork.go
@@ -79,7 +79,7 @@ func (fp *ForkingProcessor) ProcessFork(forkBlocks []*model.Block, commonBlock *
 						fp.Logger.Errorf("Failed to add blacklist: %v\n", blacklistErr)
 					}
 					fp.Logger.Warnf("[pushing fork block] failed to verify block %v from peer: %s\nwith previous: %v\n", block.ID, err, lastBlock.ID)
-					return err
+					break
 				}
 				err = fp.BlockService.PushBlock(lastBlock, block, false)
 				if err != nil {
@@ -275,18 +275,19 @@ func (fp *ForkingProcessor) restoreMempoolsBackup() error {
 		}
 		err = txType.ApplyUnconfirmed()
 		if err != nil {
-			err = fp.QueryExecutor.RollbackTx()
-			if err != nil {
-				return err
+			rollbackErr := fp.QueryExecutor.RollbackTx()
+			if rollbackErr != nil {
+				fp.Logger.Warnf("error when executing database rollback: %v", rollbackErr)
 			}
 			return err
 		}
 		err = fp.MempoolService.AddMempoolTransaction(mempoolTX)
 		if err != nil {
-			err = fp.QueryExecutor.RollbackTx()
-			if err != nil {
-				return err
+			rollbackErr := fp.QueryExecutor.RollbackTx()
+			if rollbackErr != nil {
+				fp.Logger.Warnf("error when executing database rollback: %v", rollbackErr)
 			}
+			return err
 		}
 		err = fp.QueryExecutor.CommitTx()
 		if err != nil {
