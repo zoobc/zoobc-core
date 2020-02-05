f29bbb803c931f457d371915414c35bd389a17e1
Gede Sukra Widhyawan <g.sukrawidhyawan02@gmail.com>
2020-02-05 11:30:30 +0800

    change the number of max db open connection

--- common/constant/sqlConnetion.go
+++ common/constant/sqlConnetion.go
@@ -4,9 +4,9 @@ import "time"
 
 var (
 	// SQLMaxIdleConnections Represent number of maximum idle connetion in sql pool connection
-	SQLMaxIdleConnections = 10
-	// SQLMaxConnectionLifetime Reprensent the expiration of idle database connetion
+	SQLMaxIdleConnections = 0
+	// SQLMaxConnectionLifetime Reprensent the expiration of opened database connetion
 	SQLMaxConnectionLifetime = 30 * time.Minute
 	// SQLMaxOpenConnetion represent the number of maximum open connetion to the database
-	SQLMaxOpenConnetion = 10
+	SQLMaxOpenConnetion = 40
 )
