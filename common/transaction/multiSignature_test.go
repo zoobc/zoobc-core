1a2c6d84dd1d2d0d61c5cc185e8ffd2170e29b19
andy-shi88 <shi88.andy@gmail.com>
2020-02-27 14:19:00 +0800

    #601 add multisig validation

--- common/transaction/multiSignature_test.go
+++ common/transaction/multiSignature_test.go
@@ -1,9 +1,12 @@
 package transaction
 
 import (
+	"errors"
 	"math/rand"
 	"reflect"
 	"testing"
+
+	"github.com/zoobc/zoobc-core/common/crypto"
 
 	"github.com/zoobc/zoobc-core/common/constant"
 
@@ -241,6 +244,376 @@ func TestMultiSignatureTransaction_ParseBodyBytes(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ParseBodyBytes() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+type (
+	// MultiSignatureTransactionValidate mocks
+	validateTransactionUtilParseFail struct {
+		UtilInterface
+	}
+	validateTransactionUtilParseSuccess struct {
+		UtilInterface
+	}
+	validateTypeSwitcherGetTxTypeFail struct {
+		TypeSwitcher
+	}
+	validateTypeSwitcheGetTxTypeSuccessInnerValidateFail struct {
+		TypeSwitcher
+	}
+	validateTypeSwitcheGetTxTypeSuccessInnerValidateSuccess struct {
+		TypeSwitcher
+	}
+	validateMockSendmoneyValidateFail struct {
+		TypeAction
+	}
+	validateMockSendmoneyValidateSuccess struct {
+		TypeAction
+	}
+	validateSignatureValidateFail struct {
+		crypto.Signature
+	}
+	validateSignatureValidateSuccess struct {
+		crypto.Signature
+	}
+	// MultiSignatureTransactionValidate mocks
+)
+
+func (*validateSignatureValidateFail) VerifySignature(payload, signature []byte, accountAddress string) bool {
+	return false
+}
+
+func (*validateSignatureValidateSuccess) VerifySignature(payload, signature []byte, accountAddress string) bool {
+	return true
+}
+
+func (*validateMockSendmoneyValidateFail) Validate(bool) error {
+	return errors.New("mockedError")
+}
+
+func (*validateMockSendmoneyValidateSuccess) Validate(bool) error {
+	return nil
+}
+
+func (*validateTransactionUtilParseFail) ParseTransactionBytes([]byte, bool) (*model.Transaction, error) {
+	return nil, errors.New("mockedError")
+}
+func (*validateTransactionUtilParseSuccess) ParseTransactionBytes([]byte, bool) (*model.Transaction, error) {
+	return &model.Transaction{}, nil
+}
+func (*validateTypeSwitcherGetTxTypeFail) GetTransactionType(*model.Transaction) (TypeAction, error) {
+	return nil, errors.New("mockedError")
+}
+func (*validateTypeSwitcheGetTxTypeSuccessInnerValidateSuccess) GetTransactionType(*model.Transaction) (TypeAction, error) {
+	return &validateMockSendmoneyValidateSuccess{}, nil
+}
+func (*validateTypeSwitcheGetTxTypeSuccessInnerValidateFail) GetTransactionType(*model.Transaction) (TypeAction, error) {
+	return &validateMockSendmoneyValidateFail{}, nil
+}
+func TestMultiSignatureTransaction_Validate(t *testing.T) {
+	type fields struct {
+		Body            *model.MultiSignatureTransactionBody
+		NormalFee       fee.FeeModelInterface
+		TransactionUtil UtilInterface
+		TypeSwitcher    TypeActionSwitcher
+		Signature       crypto.SignatureInterface
+	}
+	type args struct {
+		dbTx bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "MultisignatureTransaction_Validate-Fail-NothingProvided",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: nil,
+					SignatureInfo:            nil,
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    &TypeSwitcher{},
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Fail-MultisigInfoExist-Addresses<2",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo: &model.MultiSignatureInfo{
+						MinimumSignatures: 0,
+						Nonce:             0,
+						Addresses:         make([]string, 1),
+						MultisigAddress:   "",
+						BlockHeight:       0,
+					},
+					UnsignedTransactionBytes: nil,
+					SignatureInfo:            nil,
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    nil,
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Fail-MultisigInfoExist-MinimumSignature<1",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo: &model.MultiSignatureInfo{
+						MinimumSignatures: 0,
+						Nonce:             0,
+						Addresses:         make([]string, 2),
+						MultisigAddress:   "",
+						BlockHeight:       0,
+					},
+					UnsignedTransactionBytes: nil,
+					SignatureInfo:            nil,
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    nil,
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-MultisigInfoExist",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo: &model.MultiSignatureInfo{
+						MinimumSignatures: 1,
+						Nonce:             0,
+						Addresses:         make([]string, 2),
+						MultisigAddress:   "",
+						BlockHeight:       0,
+					},
+					UnsignedTransactionBytes: nil,
+					SignatureInfo:            nil,
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    nil,
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: false,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-TransactionBytesExist-FailParse",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: make([]byte, 100),
+					SignatureInfo:            nil,
+				},
+				NormalFee:       nil,
+				TransactionUtil: &validateTransactionUtilParseFail{},
+				TypeSwitcher:    nil,
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-TransactionBytesExist-FailGetType",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: make([]byte, 100),
+					SignatureInfo:            nil,
+				},
+				NormalFee:       nil,
+				TransactionUtil: &validateTransactionUtilParseSuccess{},
+				TypeSwitcher:    &validateTypeSwitcherGetTxTypeFail{},
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-TransactionBytesExist-InnerValidateFail",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: make([]byte, 100),
+					SignatureInfo:            nil,
+				},
+				NormalFee:       nil,
+				TransactionUtil: &validateTransactionUtilParseSuccess{},
+				TypeSwitcher:    &validateTypeSwitcheGetTxTypeSuccessInnerValidateFail{},
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-TransactionBytesExist-InnerValidateSuccess",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: make([]byte, 100),
+					SignatureInfo:            nil,
+				},
+				NormalFee:       nil,
+				TransactionUtil: &validateTransactionUtilParseSuccess{},
+				TypeSwitcher:    &validateTypeSwitcheGetTxTypeSuccessInnerValidateSuccess{},
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: false,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-SignatureInfoExist-TransactionhHashNil",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: nil,
+					SignatureInfo: &model.SignatureInfo{
+						TransactionHash: nil,
+						Signatures:      nil,
+					},
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    nil,
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-SignatureInfoExist-NoSignature",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: nil,
+					SignatureInfo: &model.SignatureInfo{
+						TransactionHash: make([]byte, 32),
+						Signatures:      make(map[string][]byte),
+					},
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    nil,
+				Signature:       nil,
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-SignatureInfoExist-WrongSignature",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: nil,
+					SignatureInfo: &model.SignatureInfo{
+						TransactionHash: make([]byte, 32),
+						Signatures: map[string][]byte{
+							"A": []byte{1, 2, 3},
+						},
+					},
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    nil,
+				Signature:       &validateSignatureValidateFail{},
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-SignatureInfoExist-NilSignature",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: nil,
+					SignatureInfo: &model.SignatureInfo{
+						TransactionHash: make([]byte, 32),
+						Signatures: map[string][]byte{
+							"A": nil,
+						},
+					},
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    nil,
+				Signature:       &validateSignatureValidateSuccess{},
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "MultisignatureTransaction_Validate-Success-SignatureInfoExist-ValidSignature",
+			fields: fields{
+				Body: &model.MultiSignatureTransactionBody{
+					MultiSignatureInfo:       nil,
+					UnsignedTransactionBytes: nil,
+					SignatureInfo: &model.SignatureInfo{
+						TransactionHash: make([]byte, 32),
+						Signatures: map[string][]byte{
+							"A": []byte{1, 2, 3},
+						},
+					},
+				},
+				NormalFee:       nil,
+				TransactionUtil: nil,
+				TypeSwitcher:    nil,
+				Signature:       &validateSignatureValidateSuccess{},
+			},
+			args: args{
+				dbTx: false,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := &MultiSignatureTransaction{
+				Body:            tt.fields.Body,
+				NormalFee:       tt.fields.NormalFee,
+				TransactionUtil: tt.fields.TransactionUtil,
+				TypeSwitcher:    tt.fields.TypeSwitcher,
+				Signature:       tt.fields.Signature,
+			}
+			if err := tx.Validate(tt.args.dbTx); (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
