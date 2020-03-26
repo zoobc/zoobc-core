16529661db3054eca6fa4b4195c2b8c1ce753b6a
astaphobia <ariasta.ketut@icloud.com>
2020-03-26 15:59:09 +0800

    remove part of codes that would potentially useless

--- cmd/transaction/cmd.go
+++ cmd/transaction/cmd.go
@@ -348,13 +348,6 @@ func (*TXGeneratorCommands) SetupAccountDatasetProcess() RunCommand {
 			recipientAccountAddress,
 		)
 
-		// model.AccountDatasetProperty assertion, if false just pass as string and would an
-		// model.AccountDatasetProperty key which is a string otherwise
-		accountDatasetEscrowApproval, ok := model.AccountDatasetProperty_value[property]
-		if ok {
-			property = model.AccountDatasetProperty_name[accountDatasetEscrowApproval]
-		}
-
 		tx = GenerateTxSetupAccountDataset(tx, senderAccountAddress, recipientAccountAddress, property, value, activeTime)
 		if escrow {
 			tx = GenerateEscrowedTransaction(tx)
