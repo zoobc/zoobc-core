9b94cb7183da8cfc6ba36627bcf1f1b04eb00bf3
astaphobia <ariasta.ketut@icloud.com>
2020-04-30 15:44:09 +0800

    perhaps the limiter never ever got 0, so remove the if condition

--- core/service/receiptService.go
+++ core/service/receiptService.go
@@ -451,39 +451,37 @@ func (rs *ReceiptService) PruningNodeReceipts() error {
 	}
 
 	limiter := lastBlock.GetHeight() - (2 * constant.MinRollbackBlocks)
-	if limiter > 0 {
-		removeReceiptQ, removeReceiptArgs = rs.NodeReceiptQuery.RemoveReceipts(
-			limiter,
-			constant.PruningChunkedSize,
-		)
-		removeMerkleQ, removeMerkleArgs = rs.MerkleTreeQuery.RemoveMerkleTrees(
-			limiter,
-			constant.PruningChunkedSize,
-		)
-		err = rs.QueryExecutor.BeginTx()
-		if err != nil {
-			return err
+	removeReceiptQ, removeReceiptArgs = rs.NodeReceiptQuery.RemoveReceipts(
+		limiter,
+		constant.PruningChunkedSize,
+	)
+	removeMerkleQ, removeMerkleArgs = rs.MerkleTreeQuery.RemoveMerkleTrees(
+		limiter,
+		constant.PruningChunkedSize,
+	)
+	err = rs.QueryExecutor.BeginTx()
+	if err != nil {
+		return err
+	}
+	err = rs.QueryExecutor.ExecuteTransaction(removeReceiptQ, removeReceiptArgs...)
+	if err != nil {
+		rollbackErr = rs.QueryExecutor.RollbackTx()
+		if rollbackErr != nil {
+			return rollbackErr
 		}
-		err = rs.QueryExecutor.ExecuteTransaction(removeReceiptQ, removeReceiptArgs...)
-		if err != nil {
-			rollbackErr = rs.QueryExecutor.RollbackTx()
-			if rollbackErr != nil {
-				return rollbackErr
-			}
-			return err
+		return err
+	}
+	err = rs.QueryExecutor.ExecuteTransaction(removeMerkleQ, removeMerkleArgs...)
+	if err != nil {
+		rollbackErr = rs.QueryExecutor.RollbackTx()
+		if rollbackErr != nil {
+			return rollbackErr
 		}
-		err = rs.QueryExecutor.ExecuteTransaction(removeMerkleQ, removeMerkleArgs...)
-		if err != nil {
-			rollbackErr = rs.QueryExecutor.RollbackTx()
-			if rollbackErr != nil {
-				return rollbackErr
-			}
-			return err
-		}
-		err = rs.QueryExecutor.CommitTx()
-		if err != nil {
-			return err
-		}
+		return err
+	}
+	err = rs.QueryExecutor.CommitTx()
+	if err != nil {
+		return err
 	}
 	return nil
 }
