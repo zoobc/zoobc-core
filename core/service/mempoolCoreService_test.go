2f30ce2d208e0df268a81002c01ea6df4c8684c0
capt4ce <ahmad.ali.abdilah@gmail.com>
2020-05-08 14:10:45 +0800

    remove badgerdb v1

--- core/service/mempoolCoreService_test.go
+++ core/service/mempoolCoreService_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/dgraph-io/badger"
+	"github.com/dgraph-io/badger/v2"
 	log "github.com/sirupsen/logrus"
 	"github.com/zoobc/zoobc-core/common/blocker"
 	"github.com/zoobc/zoobc-core/common/chaintype"
