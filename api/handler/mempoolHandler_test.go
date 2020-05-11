49e351a1d3aac250b598028acbf536675039582d
Nawi Kartini <nawikartini@gmail.com>
2020-05-12 02:22:02 +0800

    TestMempoolTransactionHandler_GetMempoolTransactions

--- api/handler/mempoolHandler_test.go
+++ api/handler/mempoolHandler_test.go
@@ -76,3 +76,69 @@ func TestMempoolTransactionHandler_GetMempoolTransaction(t *testing.T) {
 		})
 	}
 }
+
+type (
+	MockGetMempoolTransactionsError struct {
+		service.MempoolTransactionServiceInterface
+	}
+	MockGetMempoolTransactionsSuccess struct {
+		service.MempoolTransactionServiceInterface
+	}
+)
+
+func (*MockGetMempoolTransactionsError) GetMempoolTransactions(chainType chaintype.ChainType, params *model.GetMempoolTransactionsRequest) (*model.GetMempoolTransactionsResponse, error) {
+	return nil, errors.New("Error GetMempoolTransactions")
+}
+
+func (*MockGetMempoolTransactionsSuccess) GetMempoolTransactions(chainType chaintype.ChainType, params *model.GetMempoolTransactionsRequest) (*model.GetMempoolTransactionsResponse, error) {
+	return &model.GetMempoolTransactionsResponse{}, nil
+}
+
+func TestMempoolTransactionHandler_GetMempoolTransactions(t *testing.T) {
+	type fields struct {
+		Service service.MempoolTransactionServiceInterface
+	}
+	type args struct {
+		ctx context.Context
+		req *model.GetMempoolTransactionsRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.GetMempoolTransactionsResponse
+		wantErr bool
+	}{
+		{
+			name: "GetMempoolTransactions:Error",
+			fields: fields{
+				Service: &MockGetMempoolTransactionsError{},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetMempoolTransactions:Success",
+			fields: fields{
+				Service: &MockGetMempoolTransactionsSuccess{},
+			},
+			want:    &model.GetMempoolTransactionsResponse{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			uth := &MempoolTransactionHandler{
+				Service: tt.fields.Service,
+			}
+			got, err := uth.GetMempoolTransactions(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MempoolTransactionHandler.GetMempoolTransactions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MempoolTransactionHandler.GetMempoolTransactions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
