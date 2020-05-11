f555e97c0dfeee7ed7b16ea0cfbb781ee03f8096
Nawi Kartini <nawikartini@gmail.com>
2020-05-12 02:00:17 +0800

    TestBlockHandler_GetBlocks

--- api/handler/blockHandler_test.go
+++ api/handler/blockHandler_test.go
@@ -96,3 +96,89 @@ func TestBlockHandler_GetBlock(t *testing.T) {
 		})
 	}
 }
+
+type (
+	mockGetBlocksError struct {
+		service.BlockServiceInterface
+	}
+	mockGetBlocksSucess struct {
+		service.BlockServiceInterface
+	}
+)
+
+func (*mockGetBlocksError) GetBlocks(chainType chaintype.ChainType, Count uint32, Height uint32) (*model.GetBlocksResponse, error) {
+	return nil, errors.New("Error GetBlocks")
+}
+
+func (*mockGetBlocksSucess) GetBlocks(chainType chaintype.ChainType, Count uint32, Height uint32) (*model.GetBlocksResponse, error) {
+	return &model.GetBlocksResponse{}, nil
+}
+
+func TestBlockHandler_GetBlocks(t *testing.T) {
+	type fields struct {
+		Service service.BlockServiceInterface
+	}
+	type args struct {
+		ctx context.Context
+		req *model.GetBlocksRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.GetBlocksResponse
+		wantErr bool
+	}{
+		{
+			name: "GetBlocks:LimitExceeded",
+			args: args{
+				req: &model.GetBlocksRequest{
+					Limit: 1000,
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetBlocks:Error",
+			args: args{
+				req: &model.GetBlocksRequest{
+					Limit: 500,
+				},
+			},
+			fields: fields{
+				Service: &mockGetBlocksError{},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetBlocks:Success",
+			args: args{
+				req: &model.GetBlocksRequest{
+					Limit: 500,
+				},
+			},
+			fields: fields{
+				Service: &mockGetBlocksSucess{},
+			},
+			want:    &model.GetBlocksResponse{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bs := &BlockHandler{
+				Service: tt.fields.Service,
+			}
+			got, err := bs.GetBlocks(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BlockHandler.GetBlocks() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BlockHandler.GetBlocks() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
