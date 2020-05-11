72d3ab16307b73d3a643f482f8f54b9136a0fd59
Nawi Kartini <nawikartini@gmail.com>
2020-05-12 00:48:31 +0800

    GetAccountDatasets

---
+++ api/handler/accountDatasetHandler_test.go
@@ -0,0 +1,102 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/zoobc/zoobc-core/api/service"
+	"github.com/zoobc/zoobc-core/common/model"
+)
+
+type (
+	mockGetAccountDatasetsError struct {
+		service.AccountDatasetServiceInterface
+	}
+	mockGetAccountDatasetsSuccess struct {
+		service.AccountDatasetServiceInterface
+	}
+)
+
+func (*mockGetAccountDatasetsError) GetAccountDatasets(request *model.GetAccountDatasetsRequest) (*model.GetAccountDatasetsResponse, error) {
+	return nil, errors.New("Error GetAccountDatasets")
+}
+func (*mockGetAccountDatasetsSuccess) GetAccountDatasets(request *model.GetAccountDatasetsRequest) (*model.GetAccountDatasetsResponse, error) {
+	return &model.GetAccountDatasetsResponse{}, nil
+}
+
+func TestAccountDatasetHandler_GetAccountDatasets(t *testing.T) {
+	type fields struct {
+		Service service.AccountDatasetServiceInterface
+	}
+	type args struct {
+		in0     context.Context
+		request *model.GetAccountDatasetsRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.GetAccountDatasetsResponse
+		wantErr bool
+	}{
+		{
+			name: "GetAccountDatasets:LimitExceeded",
+			args: args{
+				request: &model.GetAccountDatasetsRequest{
+					Pagination: &model.Pagination{
+						Limit: uint32(600),
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetAccountDatasets:Error",
+			fields: fields{
+				Service: &mockGetAccountDatasetsError{},
+			},
+			args: args{
+				request: &model.GetAccountDatasetsRequest{
+					Pagination: &model.Pagination{
+						Limit: uint32(250),
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetAccountDatasets:Success",
+			fields: fields{
+				Service: &mockGetAccountDatasetsSuccess{},
+			},
+			args: args{
+				request: &model.GetAccountDatasetsRequest{
+					Pagination: &model.Pagination{
+						Limit: uint32(250),
+					},
+				},
+			},
+			want:    &model.GetAccountDatasetsResponse{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			adh := &AccountDatasetHandler{
+				Service: tt.fields.Service,
+			}
+			got, err := adh.GetAccountDatasets(tt.args.in0, tt.args.request)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AccountDatasetHandler.GetAccountDatasets() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AccountDatasetHandler.GetAccountDatasets() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
