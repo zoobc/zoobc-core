8156d9a22ffd801ecd2f5d6536e6bc58bf7b9a34
Nawi Kartini <nawikartini@gmail.com>
2020-05-13 03:26:47 +0800

    TestNodeAdminHandler_GenerateNodeKey

--- api/handler/nodeAdminHandler_test.go
+++ api/handler/nodeAdminHandler_test.go
@@ -75,3 +75,71 @@ func TestNodeAdminHandler_GetProofOfOwnership(t *testing.T) {
 		})
 	}
 }
+
+type (
+	mockGenerateNodeKeyError struct {
+		service.NodeAdminServiceInterface
+	}
+	mockGenerateNodeKeySuccess struct {
+		service.NodeAdminServiceInterface
+	}
+)
+
+func (*mockGenerateNodeKeyError) GenerateNodeKey(seed string) ([]byte, error) {
+	return nil, errors.New("Error GenerateNodeKey")
+}
+
+func (*mockGenerateNodeKeySuccess) GenerateNodeKey(seed string) ([]byte, error) {
+	return []byte(""), nil
+}
+
+func TestNodeAdminHandler_GenerateNodeKey(t *testing.T) {
+	type fields struct {
+		Service service.NodeAdminServiceInterface
+	}
+	type args struct {
+		ctx context.Context
+		req *model.GenerateNodeKeyRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.GenerateNodeKeyResponse
+		wantErr bool
+	}{
+		{
+			name: "GenerateNodeKey:Error",
+			fields: fields{
+				Service: &mockGenerateNodeKeyError{},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GenerateNodeKey:Success",
+			fields: fields{
+				Service: &mockGenerateNodeKeySuccess{},
+			},
+			want: &model.GenerateNodeKeyResponse{
+				NodePublicKey: []byte(""),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gp := &NodeAdminHandler{
+				Service: tt.fields.Service,
+			}
+			got, err := gp.GenerateNodeKey(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NodeAdminHandler.GenerateNodeKey() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NodeAdminHandler.GenerateNodeKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
