9b5bea1130b55af4055d620ab03fd49aaa577b1f
Nawi Kartini <nawikartini@gmail.com>
2020-05-13 02:27:57 +0800

    TestMultisigHandler_GetPendingTransactionDetailByTransactionHash

--- api/handler/multisigHandler_test.go
+++ api/handler/multisigHandler_test.go
@@ -101,3 +101,69 @@ func TestMultisigHandler_GetPendingTransactions(t *testing.T) {
 		})
 	}
 }
+
+type (
+	mockGetPendingTransactionDetailByTransactionHashError struct {
+		service.MultisigServiceInterface
+	}
+	mockGetPendingTransactionDetailByTransactionHashSuccess struct {
+		service.MultisigServiceInterface
+	}
+)
+
+func (*mockGetPendingTransactionDetailByTransactionHashError) GetPendingTransactionDetailByTransactionHash(param *model.GetPendingTransactionDetailByTransactionHashRequest) (*model.GetPendingTransactionDetailByTransactionHashResponse, error) {
+	return nil, errors.New("Error GetPendingTransactionDetailByTransactionHash")
+}
+
+func (*mockGetPendingTransactionDetailByTransactionHashSuccess) GetPendingTransactionDetailByTransactionHash(param *model.GetPendingTransactionDetailByTransactionHashRequest) (*model.GetPendingTransactionDetailByTransactionHashResponse, error) {
+	return &model.GetPendingTransactionDetailByTransactionHashResponse{}, nil
+}
+
+func TestMultisigHandler_GetPendingTransactionDetailByTransactionHash(t *testing.T) {
+	type fields struct {
+		MultisigService service.MultisigServiceInterface
+	}
+	type args struct {
+		ctx context.Context
+		req *model.GetPendingTransactionDetailByTransactionHashRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.GetPendingTransactionDetailByTransactionHashResponse
+		wantErr bool
+	}{
+		{
+			name: "GetPendingTransactionDetailByTransactionHash:Error",
+			fields: fields{
+				MultisigService: &mockGetPendingTransactionDetailByTransactionHashError{},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetPendingTransactionDetailByTransactionHash:Success",
+			fields: fields{
+				MultisigService: &mockGetPendingTransactionDetailByTransactionHashSuccess{},
+			},
+			want:    &model.GetPendingTransactionDetailByTransactionHashResponse{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			msh := &MultisigHandler{
+				MultisigService: tt.fields.MultisigService,
+			}
+			got, err := msh.GetPendingTransactionDetailByTransactionHash(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MultisigHandler.GetPendingTransactionDetailByTransactionHash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MultisigHandler.GetPendingTransactionDetailByTransactionHash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
