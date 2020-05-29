bc823e69ddc37ba8b09d4536b66d9b78f4e2530b
Nawi Kartini <nawikartini@gmail.com>
2020-05-29 09:37:36 +0800

    GetMorePeers

---
+++ p2p/handler/p2pServerHandler_test.go
@@ -0,0 +1,81 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/zoobc/zoobc-core/common/model"
+	service2 "github.com/zoobc/zoobc-core/p2p/service"
+)
+
+type (
+	mockGetMorePeersError struct {
+		service2.P2PServerServiceInterface
+	}
+	mockGetMorePeersSuccess struct {
+		service2.P2PServerServiceInterface
+	}
+)
+
+func (*mockGetMorePeersError) GetMorePeers(ctx context.Context, req *model.Empty) ([]*model.Node, error) {
+	return nil, errors.New("Error GetMorePeers")
+}
+
+func (*mockGetMorePeersSuccess) GetMorePeers(ctx context.Context, req *model.Empty) ([]*model.Node, error) {
+	return []*model.Node{}, nil
+}
+
+func TestP2PServerHandler_GetMorePeers(t *testing.T) {
+	type fields struct {
+		Service service2.P2PServerServiceInterface
+	}
+	type args struct {
+		ctx context.Context
+		req *model.Empty
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.GetMorePeersResponse
+		wantErr bool
+	}{
+		{
+			name: "GetMorePeers:Error",
+			fields: fields{
+				Service: &mockGetMorePeersError{},
+			},
+			args:    args{},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetMorePeers:Success",
+			fields: fields{
+				Service: &mockGetMorePeersSuccess{},
+			},
+			args: args{},
+			want: &model.GetMorePeersResponse{
+				Peers: []*model.Node{},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ss := &P2PServerHandler{
+				Service: tt.fields.Service,
+			}
+			got, err := ss.GetMorePeers(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("P2PServerHandler.GetMorePeers() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("P2PServerHandler.GetMorePeers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
