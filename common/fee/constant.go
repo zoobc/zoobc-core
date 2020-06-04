a664f2ad38aa9b654c9d543e59a802c8ba1e4596
andy-shi88 <shi88.andy@gmail.com>
2020-06-04 13:37:52 +0800

    #824 move sendmoney fee constant (initial min fee) to constant

---
+++ common/fee/constant.go
@@ -0,0 +1,8 @@
+package fee
+
+import "github.com/zoobc/zoobc-core/common/constant"
+
+const (
+	// SendMoneyFeeConstant value of initial / constant send money fee
+	SendMoneyFeeConstant = constant.OneZBC / 100
+)
