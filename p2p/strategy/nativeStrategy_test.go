332a3789251b10dd20559f153c554a600956de5c
Nawi Kartini <nawikartini@gmail.com>
2020-06-05 02:21:20 +0800

    RemoveResolvedPeer

--- p2p/strategy/nativeStrategy_test.go
+++ p2p/strategy/nativeStrategy_test.go
@@ -275,3 +275,59 @@ func TestNativeStrategy_AddToResolvedPeer(t *testing.T) {
 		})
 	}
 }
+
+func TestNativeStrategy_RemoveResolvedPeer(t *testing.T) {
+	type fields struct {
+		Host                 *model.Host
+		PeerServiceClient    client.PeerServiceClientInterface
+		ResolvedPeersLock    sync.RWMutex
+		UnresolvedPeersLock  sync.RWMutex
+		BlacklistedPeersLock sync.RWMutex
+		MaxUnresolvedPeers   int32
+		MaxResolvedPeers     int32
+		Logger               *log.Logger
+	}
+	type args struct {
+		peer *model.Peer
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "RemoveResolvedPeer:Nil",
+			wantErr: true,
+		},
+		{
+			name: "RemoveResolvedPeer:Success",
+			fields: fields{
+				Host: &model.Host{
+					ResolvedPeers: mockPeers,
+				},
+			},
+			args: args{
+				peer: &model.Peer{},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ns := &NativeStrategy{
+				Host:                 tt.fields.Host,
+				PeerServiceClient:    tt.fields.PeerServiceClient,
+				ResolvedPeersLock:    tt.fields.ResolvedPeersLock,
+				UnresolvedPeersLock:  tt.fields.UnresolvedPeersLock,
+				BlacklistedPeersLock: tt.fields.BlacklistedPeersLock,
+				MaxUnresolvedPeers:   tt.fields.MaxUnresolvedPeers,
+				MaxResolvedPeers:     tt.fields.MaxResolvedPeers,
+				Logger:               tt.fields.Logger,
+			}
+			if err := ns.RemoveResolvedPeer(tt.args.peer); (err != nil) != tt.wantErr {
+				t.Errorf("NativeStrategy.RemoveResolvedPeer() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
