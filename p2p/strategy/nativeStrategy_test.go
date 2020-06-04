fa7c2bdc0fd0e996b6ed0098a03acfdfd445c643
Nawi Kartini <nawikartini@gmail.com>
2020-06-05 06:01:13 +0800

    PeerBlacklist

--- p2p/strategy/nativeStrategy_test.go
+++ p2p/strategy/nativeStrategy_test.go
@@ -765,3 +765,58 @@ func TestNativeStrategy_GetAnyKnownPeer(t *testing.T) {
 		})
 	}
 }
+
+func TestNativeStrategy_PeerBlacklist(t *testing.T) {
+	type fields struct {
+		Host                 *model.Host
+		PeerServiceClient    client.PeerServiceClientInterface
+		ResolvedPeersLock    sync.RWMutex
+		UnresolvedPeersLock  sync.RWMutex
+		BlacklistedPeersLock sync.RWMutex
+		MaxUnresolvedPeers   int32
+		MaxResolvedPeers     int32
+		Logger               *log.Logger
+	}
+	type args struct {
+		peer  *model.Peer
+		cause string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "PeerBlacklist:Success",
+			fields: fields{
+				Host: &model.Host{
+					BlacklistedPeers: mockPeers,
+				},
+			},
+			args: args{
+				peer: &model.Peer{
+					Info: &mockNode,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ns := &NativeStrategy{
+				Host:                 tt.fields.Host,
+				PeerServiceClient:    tt.fields.PeerServiceClient,
+				ResolvedPeersLock:    tt.fields.ResolvedPeersLock,
+				UnresolvedPeersLock:  tt.fields.UnresolvedPeersLock,
+				BlacklistedPeersLock: tt.fields.BlacklistedPeersLock,
+				MaxUnresolvedPeers:   tt.fields.MaxUnresolvedPeers,
+				MaxResolvedPeers:     tt.fields.MaxResolvedPeers,
+				Logger:               tt.fields.Logger,
+			}
+			if err := ns.PeerBlacklist(tt.args.peer, tt.args.cause); (err != nil) != tt.wantErr {
+				t.Errorf("NativeStrategy.PeerBlacklist() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
