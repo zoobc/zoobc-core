1c51d017ed6ef0ce558ee187d9f1c5f17ffd5cb1
andy-shi88 <shi88.andy@gmail.com>
2020-06-24 10:08:28 +0800

    #892 basic: assign shard to node function

--- common/util/chunk.go
+++ common/util/chunk.go
@@ -2,10 +2,15 @@ package util
 
 import (
 	"math"
+	"math/big"
+	"sort"
+
+	"golang.org/x/crypto/sha3"
 )
 
 type (
 	ChunkInterface interface {
+		ShardChunk(chunks []byte, shardBitLength int) map[uint64][][]byte
 	}
 
 	Chunk struct {
@@ -36,4 +41,48 @@ func (c *Chunk) ShardChunk(chunks []byte, shardBitLength int) map[uint64][][]byt
 		shards[shardNumber] = append(shards[shardNumber], chunks[i:i+c.chunkSize])
 	}
 	return shards
+}
+
+// AssignShard assign built shard to provided nodeIDs and return the mapped data + cache to CacheStorage
+func (c *Chunk) AssignShard(
+	shards map[uint64][][]byte,
+	nodeIDs []int64,
+) (map[int64][]uint64, error) {
+	type nodeOrder struct {
+		nodeID int64
+		hash   []byte
+	}
+	var (
+		nodeShards      = make(map[int64][]uint64)
+		shardRedundancy = int(math.Ceil(math.Sqrt(float64(len(nodeIDs)))))
+	)
+	for shardNumber := range shards {
+		var nodeOrders = make([]nodeOrder, len(nodeIDs))
+		for i := 0; i < len(nodeIDs); i++ { // todo: split hashing to multiple goroutines
+			digest := sha3.New256()
+			if _, err := digest.Write(ConvertUint64ToBytes(uint64(nodeIDs[i]))); err != nil {
+				return nil, err
+			}
+			if _, err := digest.Write(ConvertUint64ToBytes(shardNumber)); err != nil {
+				return nil, err
+			}
+			nodeOrders[i] = nodeOrder{
+				nodeID: nodeIDs[i],
+				hash:   digest.Sum([]byte{}),
+			}
+		}
+		// sort nodeOrders
+		sort.SliceStable(nodeOrders, func(i, j int) bool {
+			resI := new(big.Int).SetBytes(nodeOrders[i].hash)
+			resJ := new(big.Int).SetBytes(nodeOrders[j].hash)
+			res := resI.Cmp(resJ)
+			// Ascending sort
+			return res < 0
+		})
+		for i := 0; i < shardRedundancy; i++ {
+			nodeShards[nodeOrders[i].nodeID] = append(nodeShards[nodeOrders[i].nodeID], shardNumber)
+		}
+	}
+	// todo: cache nodeShards
+	return nodeShards, nil
 }
--- common/util/chunk_test.go
+++ common/util/chunk_test.go
@@ -8,39 +8,45 @@ import (
 	"time"
 )
 
-var (
-	mockChunks = [][]byte{
-		{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{5, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{2, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{100, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{66, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{130, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{67, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{54, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{24, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		{14, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// var (
+// 	mockChunks = [][]byte{
+// 		{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{5, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{2, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{100, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{66, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{130, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{67, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{54, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{24, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 		{14, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+// 	}
+// )
+var generateRandom32Bytes = func(n int) [][]byte {
+	result := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		rand.Seed(rand.Int63())
+		token := make([]byte, 32)
+		rand.Read(token)
+		result[i] = token
 	}
-)
+	return result
+}
+var generateRandomNodeIDs = func(n int) []int64 {
+	result := make([]int64, n)
+	for i := 0; i < n; i++ {
+		result[i] = rand.Int63()
+	}
+	return result
+}
 
 func TestChunk_ShardChunk(t *testing.T) {
-	generateRandomBytes := func(n int) [][]byte {
-		result := make([][]byte, n)
-		for i := 0; i < n; i++ {
-			rand.Seed(rand.Int63())
-			token := make([]byte, 32)
-			rand.Read(token)
-			result[i] = token
-		}
-		return result
-	}
-
 	t.Run("6 shardBit-n", func(t *testing.T) {
 		const n = 1000000
 		startPrepareData := time.Now()
 		fmt.Printf("preparing %d random data\n", n)
-		mockChunks := generateRandomBytes(n)
+		mockChunks := generateRandom32Bytes(n)
 		fmt.Printf("data prepared in: %v ms\n", time.Since(startPrepareData).Milliseconds())
 		fmt.Printf("start sharding data\n")
 		startSharding := time.Now()
@@ -55,6 +61,37 @@ func TestChunk_ShardChunk(t *testing.T) {
 		fmt.Printf("finish sharding in : %v ms\n", time.Since(startSharding).Milliseconds())
 		for u, i := range result {
 			fmt.Printf("shardN: %d\tcontent: %d\n", u, len(i))
+		}
+	})
+}
+
+func TestChunk_AssignShard(t *testing.T) {
+	t.Run("assignShard - 1000 nodes", func(t *testing.T) {
+		const n = 1000000
+		startPrepareData := time.Now()
+		fmt.Printf("preparing %d random data\n", n)
+		mockChunks := generateRandom32Bytes(n)
+		fmt.Printf("data prepared in: %v ms\n", time.Since(startPrepareData).Milliseconds())
+		fmt.Printf("start sharding data\n")
+		chunk := &Chunk{
+			chunkSize: sha256.Size,
+		}
+		var chunks []byte
+		for _, mockChunk := range mockChunks {
+			chunks = append(chunks, mockChunk...)
+		}
+		startSharding := time.Now()
+		shards := chunk.ShardChunk(chunks, 6)
+		fmt.Printf("time sharding chunks: %v ms\n", time.Since(startSharding).Milliseconds())
+		nodeIDs := generateRandomNodeIDs(1000)
+		startAssignChunk := time.Now()
+		shard, err := chunk.AssignShard(shards, nodeIDs)
+		if err != nil {
+			t.Errorf("error-assigning-shard: %v", err)
+		}
+		fmt.Printf("time assigning shard: %v ms\n", time.Since(startAssignChunk).Milliseconds())
+		for i, s := range shard {
+			fmt.Printf("nodeID: %d\tnumShard: %d\n", i, len(s))
 		}
 	})
 }
