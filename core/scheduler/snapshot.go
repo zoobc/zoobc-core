4d802cee6079c3217d0093950713ed2f5352f54d
astaphobia <ariasta.ketut@icloud.com>
2020-06-30 07:32:52 +0800

    scheduler func for check integrity of snapshot latest state

--- core/scheduler/snapshot.go
+++ core/scheduler/snapshot.go
@@ -17,36 +17,44 @@ type (
 		FileService               service.FileServiceInterface
 	}
 	SnapshotSchedulerService interface {
-		CheckChunksIntegrity(chainType chaintype.ChainType)
+		CheckChunksIntegrity(chainType chaintype.ChainType, filePath string) error
 	}
 )
 
-func (ss *SnapshotScheduler) CheckChunksIntegrity(chainType chaintype.ChainType) {
+// CheckChunksIntegrity checking availability of snapshot read files from last manifest
+func (ss *SnapshotScheduler) CheckChunksIntegrity(chainType chaintype.ChainType, filePath string) error {
 	var (
 		spineBlockManifest *model.SpineBlockManifest
-		err, blockerErr    error
-		chunks             [][]byte
+		chunksHashed       [][]byte
+		err                error
 	)
 
 	spineBlockManifest, err = ss.SpineBlockManifestService.GetLastSpineBlockManifest(chainType, model.SpineBlockManifestType_Snapshot)
 	if err != nil {
 		ss.Logger.Warn(blocker.NewBlocker(blocker.SchedulerError, err.Error()))
-		return
+		return blocker.NewBlocker(blocker.SchedulerError, err.Error())
 	}
+	// NOTE: Need to check this meanwhile err checked
 	if spineBlockManifest == nil {
 		ss.Logger.Warn(blocker.NewBlocker(blocker.SchedulerError, "SpineBlockManifest is nil"))
-		return
+		return blocker.NewBlocker(blocker.SchedulerError, "SpineBlockManifest is nil")
 	}
 
-	chunks, err = ss.FileService.ParseFileChunkHashes(spineBlockManifest.GetFileChunkHashes(), sha256.Size)
+	chunksHashed, err = ss.FileService.ParseFileChunkHashes(spineBlockManifest.GetFileChunkHashes(), sha256.Size)
 	if err != nil {
 		ss.Logger.Warn(blocker.NewBlocker(blocker.SchedulerError, err.Error()))
-		return
+		return blocker.NewBlocker(blocker.SchedulerError, err.Error())
 	}
-	if len(chunks) == 0 {
+	if len(chunksHashed) != 0 {
+		for _, chunkHashed := range chunksHashed {
+			_, err = ss.FileService.ReadFileByHash(filePath, chunkHashed)
+			if err != nil {
+				ss.Logger.Warn(blocker.NewBlocker(blocker.SchedulerError, err.Error()))
+				// Could be requesting a missing chunk p2p
+			}
+		}
+	}
+	ss.Logger.Warn(blocker.NewBlocker(blocker.SchedulerError, "Failed parsing File Chunk Hashes from Spine Block Manifest"))
+	return blocker.NewBlocker(blocker.SchedulerError, "Failed parsing File Chunk Hashes from Spine Block Manifest")
 
-		blockerErr = blocker.NewBlocker(blocker.SchedulerError, "Failed parsing File Chunk Hashes from Spine Block Manifest")
-		ss.Logger.Warn(blockerErr.Error())
-		return
-	}
 }
---
+++ core/scheduler/snapshot_test.go
@@ -0,0 +1,113 @@
+package scheduler
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/ugorji/go/codec"
+	"github.com/zoobc/zoobc-core/common/chaintype"
+	"github.com/zoobc/zoobc-core/common/constant"
+	"github.com/zoobc/zoobc-core/common/model"
+	"github.com/zoobc/zoobc-core/common/transaction"
+	"github.com/zoobc/zoobc-core/core/service"
+)
+
+type (
+	mockSpineBlockManifestErr struct {
+		service.SpineBlockManifestService
+	}
+	mockSpineBlockManifestSuccess struct {
+		service.SpineBlockManifestService
+	}
+)
+
+func (*mockSpineBlockManifestErr) GetLastSpineBlockManifest(
+	chaintype.ChainType,
+	model.SpineBlockManifestType,
+) (sm *model.SpineBlockManifest, err error) {
+	return nil, sql.ErrNoRows
+}
+func (*mockSpineBlockManifestSuccess) GetLastSpineBlockManifest(
+	chaintype.ChainType,
+	model.SpineBlockManifestType,
+) (sm *model.SpineBlockManifest, err error) {
+
+	payload := model.SnapshotPayload{
+		Blocks: []*model.Block{transaction.GetFixturesForBlock(720, 1234567890)},
+	}
+
+	fullHashed, fileChunkHashes, err := service.NewSnapshotBasicChunkStrategy(constant.SnapshotChunkSize, service.NewFileService(
+		logrus.New(),
+		new(codec.CborHandle),
+		"./testdata",
+	)).GenerateSnapshotChunks(&payload, "./testdata")
+	if err != nil {
+		return nil, err
+	}
+	var fullChunkHashesx []byte
+	for _, chunkHaHash := range fileChunkHashes {
+		fullChunkHashesx = append(fullChunkHashesx, chunkHaHash...)
+	}
+	return &model.SpineBlockManifest{
+		ID:                       12345678,
+		FullFileHash:             fullHashed,
+		FileChunkHashes:          fullChunkHashesx,
+		ManifestReferenceHeight:  720,
+		ManifestSpineBlockHeight: 1,
+		ChainType:                0,
+		SpineBlockManifestType:   0,
+		ExpirationTimestamp:      567890,
+	}, nil
+}
+func TestSnapshotScheduler_CheckChunksIntegrity(t *testing.T) {
+	type fields struct {
+		Logger                    *logrus.Logger
+		SpineBlockManifestService service.SpineBlockManifestServiceInterface
+		FileService               service.FileServiceInterface
+	}
+	type args struct {
+		chainType chaintype.ChainType
+		filePath  string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "WantErr:SpineBlockManifest",
+			fields: fields{
+				Logger:                    logrus.New(),
+				SpineBlockManifestService: &mockSpineBlockManifestErr{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "WantErr:SpineBlockManifestParseFail",
+			fields: fields{
+				Logger:                    logrus.New(),
+				SpineBlockManifestService: &mockSpineBlockManifestSuccess{},
+				FileService: service.NewFileService(
+					logrus.New(),
+					new(codec.CborHandle),
+					"./testdata",
+				),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ss := &SnapshotScheduler{
+				Logger:                    tt.fields.Logger,
+				SpineBlockManifestService: tt.fields.SpineBlockManifestService,
+				FileService:               tt.fields.FileService,
+			}
+			if err := ss.CheckChunksIntegrity(tt.args.chainType, tt.args.filePath); err != nil && !tt.wantErr {
+				t.Errorf("CheckChunksIntegrity got err: %s", err.Error())
+			}
+		})
+	}
+}
