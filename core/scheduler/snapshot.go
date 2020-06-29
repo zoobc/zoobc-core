cfb229b848081f5059895176f7bb93825139beaf
astaphobia <ariasta.ketut@icloud.com>
2020-06-29 16:57:46 +0800

    no need to return error on scheduler

--- core/scheduler/snapshot.go
+++ core/scheduler/snapshot.go
@@ -17,40 +17,36 @@ type (
 		FileService               service.FileServiceInterface
 	}
 	SnapshotSchedulerService interface {
-		CheckChunksIntegrity(chainType chaintype.ChainType) error
+		CheckChunksIntegrity(chainType chaintype.ChainType)
 	}
 )
 
-func (ss *SnapshotScheduler) CheckChunksIntegrity(ct chaintype.ChainType) error {
+func (ss *SnapshotScheduler) CheckChunksIntegrity(chainType chaintype.ChainType) {
 	var (
 		spineBlockManifest *model.SpineBlockManifest
 		err, blockerErr    error
 		chunks             [][]byte
 	)
 
-	spineBlockManifest, err = ss.SpineBlockManifestService.GetLastSpineBlockManifest(ct, model.SpineBlockManifestType_Snapshot)
+	spineBlockManifest, err = ss.SpineBlockManifestService.GetLastSpineBlockManifest(chainType, model.SpineBlockManifestType_Snapshot)
 	if err != nil {
-		blockerErr = blocker.NewBlocker(blocker.SchedulerError, err.Error())
-		ss.Logger.Warn(blockerErr.Error())
-		return err
+		ss.Logger.Warn(blocker.NewBlocker(blocker.SchedulerError, err.Error()))
+		return
 	}
 	if spineBlockManifest == nil {
-		blockerErr = blocker.NewBlocker(blocker.SchedulerError, "SpineBlockManifest is nil")
-		ss.Logger.Warn(blockerErr.Error())
-		return blockerErr
+		ss.Logger.Warn(blocker.NewBlocker(blocker.SchedulerError, "SpineBlockManifest is nil"))
+		return
 	}
 
 	chunks, err = ss.FileService.ParseFileChunkHashes(spineBlockManifest.GetFileChunkHashes(), sha256.Size)
 	if err != nil {
-		blockerErr = blocker.NewBlocker(blocker.SchedulerError, err.Error())
-		ss.Logger.Warn(blockerErr.Error())
-		return blockerErr
+		ss.Logger.Warn(blocker.NewBlocker(blocker.SchedulerError, err.Error()))
+		return
 	}
 	if len(chunks) == 0 {
+
 		blockerErr = blocker.NewBlocker(blocker.SchedulerError, "Failed parsing File Chunk Hashes from Spine Block Manifest")
 		ss.Logger.Warn(blockerErr.Error())
-		return blockerErr
+		return
 	}
-
-	return nil
 }
