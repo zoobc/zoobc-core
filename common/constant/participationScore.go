2f7741b5fbd4590e979cf1de385aa76960e58e43
stefano galassi <stefano.galassi@blockchainzoo.com>
2020-07-03 10:43:48 +0800

    Code Refactoring
    * Adapded genesis and constants to alpha network

--- common/constant/participationScore.go
+++ common/constant/participationScore.go
@@ -10,7 +10,8 @@ const (
 	UnlinkedReceiptScore float32 = 0.5
 	// MaxScoreChange the maximum score that node wll get.
 	// note that in small networks if this value is too high it will lead to nodes being expelled from registry quickly
-	MaxScoreChange = 10 * int64(ScalarReceiptScore) // in production 100000000 * int64(ScalarReceiptScore)
+	// in production 100000000 * int64(ScalarReceiptScore). reduce to 10 * int64(ScalarReceiptScore) to test with less than 10 nodes
+	MaxScoreChange = 100000000 * int64(ScalarReceiptScore)
 	// punishment amount
 	ParticipationScorePunishAmount = -1 * MaxScoreChange / 2
 	// MaxReceipt the maximum receipt will publish in every block
