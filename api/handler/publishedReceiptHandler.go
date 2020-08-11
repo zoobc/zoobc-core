c25ba1315d28df54a135f3940b2754cfab6233b2
andy-shi88 <shi88.andy@gmail.com>
2020-08-11 20:26:58 +0800

    #1040 add limit checking

--- api/handler/publishedReceiptHandler.go
+++ api/handler/publishedReceiptHandler.go
@@ -3,7 +3,10 @@ package handler
 import (
 	"context"
 	"github.com/zoobc/zoobc-core/api/service"
+	"github.com/zoobc/zoobc-core/common/constant"
 	"github.com/zoobc/zoobc-core/common/model"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // PublishedReceiptHandler handles requests related to published receipts
@@ -20,6 +23,15 @@ func (prh *PublishedReceiptHandler) GetPublishedReceipts(
 		response *model.GetPublishedReceiptsResponse
 		err      error
 	)
+	if req.GetFromHeight() > req.GetToHeight() {
+		return nil, status.Errorf(
+			codes.FailedPrecondition,
+			"ToHeight should bigger than FromHeight",
+		)
+	}
+	if req.GetToHeight()-req.GetFromHeight() > constant.MaxAPILimitPerPage {
+		return nil, status.Errorf(codes.OutOfRange, "Limit exceeded, max. %d", constant.MaxAPILimitPerPage)
+	}
 	response, err = prh.Service.GetPublishedReceipts(req)
 	if err != nil {
 		return nil, err
