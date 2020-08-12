676151e4862d573fe2b0543d8005e46ea0e7a9f7
andy-shi88 <shi88.andy@gmail.com>
2020-08-12 17:52:42 +0800

    fix duplicate participant fetch, causing ApplyConfirm fail

--- common/query/multisignatureInfoQuery_test.go
+++ common/query/multisignatureInfoQuery_test.go
@@ -199,7 +199,7 @@ func TestMultisignatureInfoQuery_GetMultisignatureInfoByAddress(t *testing.T) {
 			},
 			wantStr: "SELECT multisig_address, minimum_signatures, nonce, block_height, latest, " +
 				"(SELECT GROUP_CONCAT(account_address, ',') FROM " +
-				"multisignature_participant WHERE multisig_address = ? GROUP BY multisig_address, block_height " +
+				"multisignature_participant WHERE multisig_address = ? AND latest = true GROUP BY multisig_address, block_height " +
 				"ORDER BY account_address_index DESC) as addresses " +
 				"FROM multisignature_info WHERE multisig_address = ? AND block_height >= ? AND latest = true",
 			wantArgs: []interface{}{"A", "A", uint32(0)},
