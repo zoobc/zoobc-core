045c13758d1cb0e636a06b38e643a6b46b711c78
nawikart <nawikartini@live.com>
2020-08-25 15:23:43 +0800

    GetEscrowTransaction

--- api/handler/escrowTransactionHandler_test.go
+++ api/handler/escrowTransactionHandler_test.go
@@ -77,3 +77,70 @@ func TestEscrowTransactionHandler_GetEscrowTransactions(t *testing.T) {
 		})
 	}
 }
+
+type (
+	mockGetEscrowTransactionError struct {
+		service.EscrowTransactionServiceInterface
+	}
+	mockGetEscrowTransactionSuccess struct {
+		service.EscrowTransactionServiceInterface
+	}
+)
+
+func (*mockGetEscrowTransactionError) GetEscrowTransaction(request *model.GetEscrowTransactionRequest,
+) (*model.Escrow, error) {
+	return nil, errors.New("Error GetEscrowTransaction")
+}
+func (*mockGetEscrowTransactionSuccess) GetEscrowTransaction(request *model.GetEscrowTransactionRequest,
+) (*model.Escrow, error) {
+	return &model.Escrow{}, nil
+}
+
+func TestEscrowTransactionHandler_GetEscrowTransaction(t *testing.T) {
+	type fields struct {
+		Service service.EscrowTransactionServiceInterface
+	}
+	type args struct {
+		in0 context.Context
+		req *model.GetEscrowTransactionRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.Escrow
+		wantErr bool
+	}{
+		{
+			name: "GetEscrowTransaction:Error",
+			fields: fields{
+				Service: &mockGetEscrowTransactionError{},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetEscrowTransaction:Success",
+			fields: fields{
+				Service: &mockGetEscrowTransactionSuccess{},
+			},
+			want:    &model.Escrow{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			eh := &EscrowTransactionHandler{
+				Service: tt.fields.Service,
+			}
+			got, err := eh.GetEscrowTransaction(tt.args.in0, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EscrowTransactionHandler.GetEscrowTransaction() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("EscrowTransactionHandler.GetEscrowTransaction() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
