f90b3af8aa756cd9d936f989557687b688f4c19c
andy-shi88 <shi88.andy@gmail.com>
2020-08-25 10:25:29 +0800

    #1087 start node smithing checks wether or not is in node_registry

--- core/smith/blockchainProcessor.go
+++ core/smith/blockchainProcessor.go
@@ -1,6 +1,7 @@
 package smith
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/zoobc/zoobc-core/common/chaintype"
@@ -31,6 +32,7 @@ type (
 		Logger                  *log.Logger
 		smithError              error
 		BlockchainStatusService service.BlockchainStatusServiceInterface
+		NodeRegistrationService service.NodeRegistrationServiceInterface
 	}
 )
 
@@ -45,6 +47,7 @@ func NewBlockchainProcessor(
 	blockService service.BlockServiceInterface,
 	logger *log.Logger,
 	blockchainStatusService service.BlockchainStatusServiceInterface,
+	nodeRegistrationService service.NodeRegistrationServiceInterface,
 ) *BlockchainProcessor {
 	return &BlockchainProcessor{
 		ChainType:               ct,
@@ -52,6 +55,7 @@ func NewBlockchainProcessor(
 		BlockService:            blockService,
 		Logger:                  logger,
 		BlockchainStatusService: blockchainStatusService,
+		NodeRegistrationService: nodeRegistrationService,
 	}
 }
 
@@ -130,6 +134,15 @@ func (bp *BlockchainProcessor) FakeSmithing(numberOfBlocks int, fromGenesis bool
 
 // StartSmithing start smithing loop
 func (bp *BlockchainProcessor) StartSmithing() error {
+	if bp.Generator.NodeID == 0 {
+		node, err := bp.NodeRegistrationService.GetNodeRegistrationByNodePublicKey(bp.Generator.NodePublicKey)
+		if err != nil {
+			return blocker.NewBlocker(blocker.AppErr, fmt.Sprintf("fail-GetNodeRegistrationByNodePublicKey: %v", err))
+		} else if node == nil {
+			return blocker.NewBlocker(blocker.ValidationErr, "BlocksmithNotInRegistry")
+		}
+		bp.Generator.NodeID = node.NodeID
+	}
 	// Securing smithing process
 	// will pause another process that used block service lock until this process done
 	bp.BlockService.ChainWriteLock(constant.BlockchainStatusGeneratingBlock)
