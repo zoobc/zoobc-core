0f39cbf2ed423b63fbaf7222670c43831bd3998d
nawikart <nawikartini@live.com>
2020-08-25 15:01:59 +0800

    GetEscrowTransactions

---
+++ api/handler/escrowTransactionHandler_test.go
@@ -0,0 +1,79 @@
+package handler
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/zoobc/zoobc-core/api/service"
+	"github.com/zoobc/zoobc-core/common/model"
+)
+
+type (
+	mockGetEscrowTransactionsFailed struct {
+		service.EscrowTransactionServiceInterface
+	}
+	mockGetEscrowTransactionsSuccess struct {
+		service.EscrowTransactionServiceInterface
+	}
+)
+
+func (*mockGetEscrowTransactionsFailed) GetEscrowTransactions(request *model.GetEscrowTransactionsRequest,
+) (*model.GetEscrowTransactionsResponse, error) {
+	return nil, errors.New("Error GetEscrowTransactions")
+}
+
+func (*mockGetEscrowTransactionsSuccess) GetEscrowTransactions(request *model.GetEscrowTransactionsRequest,
+) (*model.GetEscrowTransactionsResponse, error) {
+	return &model.GetEscrowTransactionsResponse{}, nil
+}
+
+func TestEscrowTransactionHandler_GetEscrowTransactions(t *testing.T) {
+	type fields struct {
+		Service service.EscrowTransactionServiceInterface
+	}
+	type args struct {
+		in0 context.Context
+		req *model.GetEscrowTransactionsRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.GetEscrowTransactionsResponse
+		wantErr bool
+	}{
+		{
+			name: "GetEscrowTransactions:Error",
+			fields: fields{
+				Service: &mockGetEscrowTransactionsFailed{},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GetEscrowTransactions:Success",
+			fields: fields{
+				Service: &mockGetEscrowTransactionsSuccess{},
+			},
+			want:    &model.GetEscrowTransactionsResponse{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			eh := &EscrowTransactionHandler{
+				Service: tt.fields.Service,
+			}
+			got, err := eh.GetEscrowTransactions(tt.args.in0, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EscrowTransactionHandler.GetEscrowTransactions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("EscrowTransactionHandler.GetEscrowTransactions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
