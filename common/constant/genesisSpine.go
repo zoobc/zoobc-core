fb30f81b975f5da1896225c7b50f02aec68c814a
Gede Sukra Widhyawan <g.sukrawidhyawan02@gmail.com>
2020-08-31 16:10:44 +0800

    revert SpinechainGenesisBlockTimestamp

--- common/constant/genesisSpine.go
+++ common/constant/genesisSpine.go
@@ -12,7 +12,7 @@ var (
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	SpinechainGenesisTransactionSignature = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	SpinechainGenesisBlockTimestamp = int64(1597384200000)
+	SpinechainGenesisBlockTimestamp = int64(1596708000)
 	SpinechainGenesisAccountAddress = "ZBC_AQTEH7K4_L45WJPLL_HCEC65ZH_7XC5N3XD_YNKPHK45_POH7PQME_AFAFBDWM"
 	SpinechainGenesisBlockSeed      = make([]byte, 64)
 	SpinechainGenesisNodePublicKey  = make([]byte, 32)
