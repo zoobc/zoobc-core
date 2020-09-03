c034803aa33776a3218cdf7b48959f265e54346f
Gede Sukra Widhyawan <g.sukrawidhyawan02@gmail.com>
2020-09-03 10:43:33 +0800

    set default cliMonitoring to false

--- common/util/loadConfig.go
+++ common/util/loadConfig.go
@@ -26,7 +26,7 @@ func LoadConfig(path, name, extension string) error {
 	viper.SetDefault("logLevels", []string{"fatal", "error", "panic"})
 	viper.SetDefault("snapshotPath", "./resource/snapshots")
 	viper.SetDefault("logOnCli", false)
-	viper.SetDefault("cliMonitoring", true)
+	viper.SetDefault("cliMonitoring", false)
 	viper.SetDefault("maxAPIRequestPerSecond", 10)
 
 	viper.SetEnvPrefix("zoobc") // will be uppercased automatically
