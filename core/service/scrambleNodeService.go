b37606583d120c7a8b2d11c07b3bab8b15bae488
andy-shi88 <shi88.andy@gmail.com>
2020-09-23 09:50:47 +0800

    #1189 handle scramble node cache error

--- core/service/scrambleNodeService.go
+++ core/service/scrambleNodeService.go
@@ -140,10 +140,10 @@ func (sns *ScrambleNodeService) PopOffScrambleToHeight(height uint32) error {
 	}
 	nearestScrambleHeight := sns.GetBlockHeightToBuildScrambleNodes(height)
 	index = (nearestScrambleHeight - firstCachedScramble.BlockHeight) / constant.PriorityStrategyBuildScrambleNodesGap
-	if index >= 0 {
+	if index > 0 {
 		err = sns.cacheStorage.PopTo(index - 1)
 	} else {
-		sns.cacheStorage.PopTo(0)
+		err = sns.cacheStorage.PopTo(0)
 	}
 	return err
 }
