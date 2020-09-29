824d99d3df5d7ee036daf8c33fef46aad050731f
astaphobia <ariasta.ketut@icloud.com>
2020-09-29 19:57:51 +0800

    fix linter error

--- common/transaction/multiSignature.go
+++ common/transaction/multiSignature.go
@@ -815,7 +815,7 @@ func (tx *MultiSignatureTransaction) EscrowValidate(dbTx bool) error {
 	if tx.Escrow.GetCommission() <= 0 {
 		return blocker.NewBlocker(blocker.RequestParameterErr, "CommissionRequired")
 	}
-	if tx.Escrow.GetTimeout() <= 0 {
+	if tx.Escrow.GetTimeout() > uint64(constant.MinRollbackBlocks) {
 		return blocker.NewBlocker(blocker.ValidationErr, "TimeoutRequired")
 	}
 	err = tx.Validate(dbTx)
