436f36b8fa9dbb7134af07e48d5715a726dec767
astaphobia <ariasta.ketut@icloud.com>
2020-10-02 13:12:16 +0800

    fix no save node shard ever

--- common/storage/nodeShardCacheStorage.go
+++ common/storage/nodeShardCacheStorage.go
@@ -78,6 +78,7 @@ func (n *NodeShardCacheStorage) GetItem(lastChange, item interface{}) error {
 				copy(shardMapCopy.ShardChunks[u], i)
 			}
 		}
+		// return blocker.NewBlocker(blocker.NotFound, "item not found")
 		return nil
 	}
 	return blocker.NewBlocker(blocker.ValidationErr, "WrongType lastChange")
