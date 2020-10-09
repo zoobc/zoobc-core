efc964496d17544fc0534cb047fc55db4e0d0cfe
astaphobia <ariasta.ketut@icloud.com>
2020-10-09 16:22:03 +0800

    check duplication first before validate the receipt

--- core/service/receiptService_test.go
+++ core/service/receiptService_test.go
@@ -10,6 +10,8 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/zoobc/zoobc-core/common/blocker"
+
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/zoobc/zoobc-core/common/chaintype"
 	"github.com/zoobc/zoobc-core/common/constant"
@@ -1134,6 +1136,7 @@ func TestReceiptService_IsDuplicated(t *testing.T) {
 			},
 			args:           args{datumHash: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9}, publicKey: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9}},
 			wantDuplicated: true,
+			wantErr:        true,
 		},
 		{
 			name: "want:Success",
@@ -1159,13 +1162,15 @@ func TestReceiptService_IsDuplicated(t *testing.T) {
 				MainBlockStateStorage:   tt.fields.MainBlockStateStorage,
 				ReceiptReminderStorage:  tt.fields.ReceiptReminderStorage,
 			}
-			gotDuplicated, err := rs.IsDuplicated(tt.args.publicKey, tt.args.datumHash)
+			err := rs.CheckDuplication(tt.args.publicKey, tt.args.datumHash)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("IsDuplicated() error = %v, wantErr %v", err, tt.wantErr)
+				b := err.(blocker.Blocker)
+				if tt.wantDuplicated && b.Type != blocker.DuplicateReceiptErr {
+					t.Errorf("CheckDuplication() gotDuplicated = %v, want %v", b.Type, tt.wantDuplicated)
+					return
+				}
+				t.Errorf("CheckDuplication() error = %v, wantErr %v", err, tt.wantErr)
 				return
-			}
-			if gotDuplicated != tt.wantDuplicated {
-				t.Errorf("IsDuplicated() gotDuplicated = %v, want %v", gotDuplicated, tt.wantDuplicated)
 			}
 		})
 	}
