93f33b05c12ab08ac6a03b9237d84f334495752b
stefano galassi <stefano.galassi@blockchainzoo.com>
2020-10-14 14:59:53 +0800

    Bug fix
    * Fix lint errors

--- common/query/multisignatureInfoQuery.go
+++ common/query/multisignatureInfoQuery.go
@@ -223,7 +223,6 @@ func (msi *MultisignatureInfoQuery) BuildModel(
 		var (
 			multisigInfo model.MultiSignatureInfo
 		)
-		rows.Columns()
 		err := rows.Scan(
 			&multisigInfo.MultisigAddress,
 			&multisigInfo.MinimumSignatures,
@@ -298,17 +297,6 @@ func (msi *MultisignatureInfoQuery) SelectDataForSnapshot(fromHeight, toHeight u
 		fromHeight,
 		toHeight,
 	)
-	//
-	// return fmt.Sprintf(
-	// 	"SELECT %s, %s FROM %s WHERE (multisig_address, block_height) IN (SELECT t2.multisig_address, MAX(t2.block_height) FROM %s t2 WHERE t2.block_height >= %d AND t2.block_height <= %d AND t2.block_height != 0 GROUP BY t2.multisig_address) ORDER BY block_height, account_address_index",
-	// 	strings.Join(msi.Fields, ", "),
-	// 	"(SELECT GROUP_CONCAT(account_address, ',') FROM multisignature_participant GROUP BY multisig_address, block_height "+
-	// 		"ORDER BY account_address_index ASC) as addresses",
-	// 	msi.getTableName(),
-	// 	msi.getTableName(),
-	// 	fromHeight,
-	// 	toHeight,
-	// )
 }
 
 // TrimDataBeforeSnapshot delete entries to assure there are no duplicates before applying a snapshot
