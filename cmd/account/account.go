f7b84e99d1bb768b4511b599d259c7f13f8eab90
astaphobia <ariasta.ketut@icloud.com>
2020-10-22 08:53:33 +0800

    fix typo naming

--- cmd/account/account.go
+++ cmd/account/account.go
@@ -38,11 +38,11 @@ private key both in bytes and hex representation + the secret phrase
 		Use:   "ed25519",
 		Short: "Generate account using ed25519 algorithm. This is the default zoobc account",
 	}
-	bitcoinAccuntCmd = &cobra.Command{
+	bitcoinAccountCmd = &cobra.Command{
 		Use:   "bitcoin",
 		Short: "Generate account based on Bitcoin signature that using Elliptic Curve Digital Signature Algorithm",
 	}
-	convAccuntToHexCmd = &cobra.Command{
+	convAccountToHexCmd = &cobra.Command{
 		Use:   "hexconv",
 		Short: "Convert a given (encoded/string) account address to hex format",
 	}
@@ -62,17 +62,17 @@ func init() {
 	ed25519AccountCmd.Flags().StringVar(&seed, "seed", "", "Seed that is used to generate the account")
 	ed25519AccountCmd.Flags().BoolVar(&ed25519UseSlip10, "use-slip10", false, "use slip10 to generate ed25519 private key")
 	// bitcoin
-	bitcoinAccuntCmd.Flags().StringVar(&seed, "seed", "", "Seed that is used to generate the account")
-	bitcoinAccuntCmd.Flags().Int32Var(
+	bitcoinAccountCmd.Flags().StringVar(&seed, "seed", "", "Seed that is used to generate the account")
+	bitcoinAccountCmd.Flags().Int32Var(
 		&bitcoinPrivateKeyLength,
 		"private-key-length",
 		int32(model.PrivateKeyBytesLength_PrivateKey256Bits),
 		"The length of private key Bitcoin want to generate. supported format are 32, 48 & 64 length",
 	)
-	convAccuntToHexCmd.Flags().StringVar(&encodedAccountAddress, "encodedAccountAddress", "",
+	convAccountToHexCmd.Flags().StringVar(&encodedAccountAddress, "encodedAccountAddress", "",
 		"formatted/encoded account address. eg. ZBC_F5YUYDXD_WFDJSAV5_K3Y72RCM_GLQP32XI_QDVXOGGD_J7CGSSSK_5VKR7YML")
-	convAccuntToHexCmd.Flags().Int32Var(&accountTypeInt, "accountType", 0, "Account type num: 0=default, 1=btc, etc..")
-	bitcoinAccuntCmd.Flags().Int32Var(
+	convAccountToHexCmd.Flags().Int32Var(&accountTypeInt, "accountType", 0, "Account type num: 0=default, 1=btc, etc..")
+	bitcoinAccountCmd.Flags().Int32Var(
 		&bitcoinPublicKeyFormat,
 		"public-key-format",
 		int32(model.BitcoinPublicKeyFormat_PublicKeyFormatCompressed),
@@ -96,10 +96,10 @@ func Commands() *cobra.Command {
 	}
 	ed25519AccountCmd.Run = accountGeneratorInstance.GenerateEd25519Account()
 	accountCmd.AddCommand(ed25519AccountCmd)
-	bitcoinAccuntCmd.Run = accountGeneratorInstance.GenerateBitcoinAccount()
-	accountCmd.AddCommand(bitcoinAccuntCmd)
-	convAccuntToHexCmd.Run = accountGeneratorInstance.ConvertEncodedAccountAddressToHex()
-	accountCmd.AddCommand(convAccuntToHexCmd)
+	bitcoinAccountCmd.Run = accountGeneratorInstance.GenerateBitcoinAccount()
+	accountCmd.AddCommand(bitcoinAccountCmd)
+	convAccountToHexCmd.Run = accountGeneratorInstance.ConvertEncodedAccountAddressToHex()
+	accountCmd.AddCommand(convAccountToHexCmd)
 	multiSigCmd.Run = accountGeneratorInstance.GenerateMultiSignatureAccount()
 	accountCmd.AddCommand(multiSigCmd)
 	return accountCmd
