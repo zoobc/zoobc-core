cf8f398060b14e81c76abaafa525ef47a70c5aa2
andy-shi88 <shi88.andy@gmail.com>
2020-10-26 10:51:03 +0800

    log node_id-address:port of failed nodeAddressInfo update

--- core/service/nodeAddressInfoService.go
+++ core/service/nodeAddressInfoService.go
@@ -594,7 +594,8 @@ func (nru *NodeAddressInfoService) UpdateOrInsertAddressInfo(
 		}
 		err = nru.UpdateAddrressInfo(nodeAddressInfo)
 		if err != nil {
-			nru.Logger.Errorf("UpdateAddressInfoFail - %v", err)
+			nru.Logger.Errorf("UpdateAddressInfoFail - %v - nodeAddressInfo:%d-%s:%d\n",
+				err, nodeAddressInfo.NodeID, nodeAddressInfo.Address, nodeAddressInfo.Port)
 			return false, err
 		}
 		return true, nil
