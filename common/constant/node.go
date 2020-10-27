86c67548a9f927c350225887e70ff4f16a4f0b13
stefano galassi <stefano.galassi@blockchainzoo.com>
2020-10-27 11:29:52 +0800

    Code Refactoring
    * update NodeAdmissionGenesisDelay to 3 weeks

--- common/constant/node.go
+++ common/constant/node.go
@@ -5,7 +5,7 @@ var (
 	ProofOfOwnershipExpiration uint32 = 100
 	MaxNodeAdmittancePerCycle  uint32 = 1
 
-	NodeAdmissionGenesisDelay int64 = 0            // TODO: revert after demo ,  3 months in production
+	NodeAdmissionGenesisDelay int64 = 21 * OneDay  // 3 weeks for beta network
 	NodeAdmissionBaseDelay    int64 = OneHour      // 1 year in production
 	NodeAdmissionMinDelay     int64 = 60           // 1 hour in production
 	NodeAdmissionMaxDelay     int64 = 72 * OneHour // 72 hours in production
