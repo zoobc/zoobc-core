63e6791bc7b90b0f951644bd6825cf5a7178e6e3
stefano galassi <stefano.galassi@blockchainzoo.com>
2020-12-02 16:48:29 +0800

    Code Refactoring
    * Increase max tx per block to 500
    * Increase max idle conn to 20
    * Increase max open conn to 100

--- common/constant/sqlConnetion.go
+++ common/constant/sqlConnetion.go
@@ -4,11 +4,11 @@ import "time"
 
 var (
 	// SQLMaxIdleConnections Represent number of maximum idle connetion in sql pool connection
-	SQLMaxIdleConnections = 10
+	SQLMaxIdleConnections = 20
 	// SQLMaxConnectionLifetime Reprensent the expiration of opened database connetion
 	SQLMaxConnectionLifetime = 30 * time.Minute
 	// SQLMaxOpenConnetion represent the number of maximum open connetion to the database
-	SQLMaxOpenConnetion = 50
+	SQLMaxOpenConnetion = 100
 	// SQLiteLimitVariableNumber equivalent to SQLITE_LIMIT_VARIABLE_NUMBER from sqlite
 	SQLiteLimitVariableNumber = 999
 )
