ee31366001713e8d56ca970db004f39d83037213
stefano galassi <stefano.galassi@blockchainzoo.com>
2021-01-06 16:19:00 +0800

    Code Refactoring
    * Update sql open connection parameters for testing

--- common/constant/sqlConnetion.go
+++ common/constant/sqlConnetion.go
@@ -55,9 +55,9 @@ var (
 	// SQLMaxIdleConnections Represent number of maximum idle connetion in sql pool connection
 	SQLMaxIdleConnections = 10
 	// SQLMaxConnectionLifetime Reprensent the expiration of opened database connetion
-	SQLMaxConnectionLifetime = 30 * time.Minute
+	SQLMaxConnectionLifetime = 1 * time.Minute
 	// SQLMaxOpenConnetion represent the number of maximum open connetion to the database
-	SQLMaxOpenConnetion = 2
+	SQLMaxOpenConnetion = 10
 	// SQLiteLimitVariableNumber equivalent to SQLITE_LIMIT_VARIABLE_NUMBER from sqlite
 	SQLiteLimitVariableNumber = 999
 )
