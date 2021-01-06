249d4dda0dc31b55f2e415d6adb349889c2badcb
stefano galassi <stefano.galassi@blockchainzoo.com>
2021-01-06 16:35:22 +0800

    Code Refactoring
    * Update SQLMaxConnectionLifetime

--- common/constant/sqlConnetion.go
+++ common/constant/sqlConnetion.go
@@ -55,7 +55,7 @@ var (
 	// SQLMaxIdleConnections Represent number of maximum idle connetion in sql pool connection
 	SQLMaxIdleConnections = 10
 	// SQLMaxConnectionLifetime Reprensent the expiration of opened database connetion
-	SQLMaxConnectionLifetime = 1 * time.Minute
+	SQLMaxConnectionLifetime = 10 * time.Minute
 	// SQLMaxOpenConnetion represent the number of maximum open connetion to the database
 	SQLMaxOpenConnetion = 10
 	// SQLiteLimitVariableNumber equivalent to SQLITE_LIMIT_VARIABLE_NUMBER from sqlite
