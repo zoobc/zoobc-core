876e2189574aff74c5ff4c1e3916dc2f0a1e3942
stefano galassi <stefano.galassi@blockchainzoo.com>
2021-01-07 11:13:07 +0800

    Code Refactoring
    * Add priorityPreferenceLock variable to main so we can reuse the same lock application wide

--- main.go
+++ main.go
@@ -164,6 +164,7 @@ var (
 	feedbackStrategy                                                       feedbacksystem.FeedbackStrategyInterface
 	blocksmithStrategyMain                                                 strategy.BlocksmithStrategyInterface
 	blocksmithStrategySpine                                                strategy.BlocksmithStrategyInterface
+	priorityPreferenceLock                                                 queue.PriorityLock
 )
 var (
 	flagConfigPath, flagConfigPostfix, flagResourcePath string
@@ -343,7 +344,7 @@ func initiateMainInstance() {
 	if err != nil {
 		loggerCoreService.Fatal(err)
 	}
-	queryExecutor = query.NewQueryExecutor(db, queue.NewPriorityPreferenceLock())
+	queryExecutor = query.NewQueryExecutor(db, priorityPreferenceLock)
 
 	// initialize cache storage
 	mainBlockStateStorage = storage.NewBlockStateStorage()
@@ -1255,6 +1256,7 @@ func init() {
 	rootCmd.PersistentFlags().BoolVar(&flagUseEnv, "use-env", false, "Running node without configuration file")
 	rootCmd.PersistentFlags().StringVar(&flagResourcePath, "resource-path", "", "Resource path location")
 	config = model.NewConfig()
+	priorityPreferenceLock = queue.NewPriorityPreferenceLock()
 }
 
 func main() {
