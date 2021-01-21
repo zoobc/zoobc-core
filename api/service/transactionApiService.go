6dafed027668aaecc104a068c10b00cd4c250a43
kevinh2810 <kevinhadisutanto@gmail.com>
2021-01-21 23:35:58 +0700

    add params to get Tx by BlockHeight

--- api/service/transactionApiService.go
+++ api/service/transactionApiService.go
@@ -201,6 +201,17 @@ func (ts *TransactionService) GetTransactions(
 	}
 	selectQuery, args = caseQuery.Build()
 
+	fromBlock := params.GetFromBlock()
+	toBlock := params.GetToBlock()
+
+	if fromBlock < toBlock {
+		return nil, status.Error(codes.Internal, "FromBlock height cannot excedd toBlock height")
+	}
+
+	if toBlock != 0 && fromBlock <= toBlock {
+		caseQuery.And(caseQuery.Between("height", fromBlock, toBlock))
+	}
+
 	// count first
 	countQuery := query.GetTotalRecordOfSelect(selectQuery)
 	rowCount, err = ts.Query.ExecuteSelectRow(countQuery, false, args...)
