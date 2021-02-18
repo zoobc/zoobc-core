2910834ed48d9a9cf7e2ccadcad3b8429b9c573a
Ahmad Ali Abdilah <ahmad.ali.abdilah@gmail.com>
2021-02-18 13:39:58 +0800

    Change account address required in config file to an encoded address (#1469)
    * refactor load configuration func to accept encoded address in config
    
    * update config files
    
    * remove unnecessary check in LoadConfig
    
    * fix failing tests
    
    * fix lint error

--- api/client/GetTransactionMinimumFee/client.go
+++ api/client/GetTransactionMinimumFee/client.go
@@ -55,7 +55,6 @@ import (
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 	rpc_model "github.com/zoobc/zoobc-core/common/model"
 	rpc_service "github.com/zoobc/zoobc-core/common/service"
 	"github.com/zoobc/zoobc-core/common/util"
@@ -63,19 +62,17 @@ import (
 )
 
 func main() {
-	var (
-		ip string
-	)
+	var ip string
 	flag.StringVar(&ip, "ip", "", "Usage")
 	flag.Parse()
 	if len(ip) < 1 {
-		if err := util.LoadConfig("../../../", "config", "toml", ""); err != nil {
+		config, err := util.LoadConfig("../../../", "config", "toml", "")
+		if err != nil {
 			log.Fatal(err)
 		} else {
-			ip = fmt.Sprintf(":%d", viper.GetInt("apiRPCPort"))
+			ip = fmt.Sprintf(":%d", config.RPCAPIPort)
 		}
 	}
-
 	conn, err := grpc.Dial(ip, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %s", err)
